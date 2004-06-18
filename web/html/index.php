79f64a059254bbbae93cf7c2f9dfa512aa5f9f7e
eric <eric>
2004-06-18 00:25:15 +0000

    genpopo seems to do its job now

--- web/html/index.php
+++ web/html/index.php
@@ -1,3 +1,9 @@
 <?
-print "Hello, world!<br/>\n";
+include_once("index_po.inc");
+
+print _("Hello, world!")."<br/>\n";
+print _("Hello, again!")."<br/>\n";
+print _("Julie was here!")."<br/>\n";
+print _("Abernathy was here!")."<br/>\n";
+
 ?>
