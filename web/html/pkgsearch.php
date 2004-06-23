252855ad9cbc9667faba0627680333137d70da9f
eric <eric>
2004-06-23 18:54:50 +0000

    possibly merge some scripts?

--- web/html/pkgsearch.php
+++ web/html/pkgsearch.php
@@ -4,11 +4,38 @@ include("search_po.inc");   # use some form of this for i18n support
 set_lang();                 # this sets up the visitor's language
 check_sid();                # see if they're still logged in
 html_header();              # print out the HTML header
-
-
-# Any text you print out to the visitor, use the __() function
-# for i18n support.  See 'testpo.php' for more details.
+	
+# TODO Maybe pkgsearch, pkgvote can be consolidated?  This script can
+# provide a search form.  In the results, it can contain a checkbox
+# for 'flag out of date', 'vote', 'details' link, and a link to 'pkgmgmnt'.
 #
+# the results page should have columns for,
+# pkg name/ver, location, maintainer, description, O-O-D, Vote, details, mgmnt
+#
+
+
+# get login privileges
+#
+if (isset($_COOKIE["AURSID"])) {
+	# Only logged in users can do stuff
+	#
+	$atype = account_from_sid($_COOKIE["AURSID"]);
+} else {
+	$atype = "";
+}
+
+
+if ($atype && $_REQUEST["Action"] == "Something") {
+	# do something based on what the user specifies
+	#
+} elseif ($atype && $_REQUEST["Action"] == "SomethingElse") {
+	# do something else based on what the user specifies
+	#
+} else {
+	# do the default thing - give the user a search form that they
+	# can specify: location, category, maintainer, name, 'my pkgs'
+	#
+}
 print __("Under construction...")."<br/>\n";
 
 
