45e5883a1579ef64071d161a216e7159bfab1daf
simo <simo>
2005-06-10 01:40:22 +0000

    Added newpackage-notify to finally implement new package notifications that people can ask for on their user page. This script should run on a daily cronjob.
    A few spelling stupidity fixes as well.

--- web/html/pkgedit.php
+++ web/html/pkgedit.php
@@ -93,7 +93,7 @@ if ($_REQUEST["add_Comment"]) {
 			$q.= "WHERE Packages.ID = ".intval($_REQUEST["ID"]);
 			$result = db_query($q, $dbh);
 			$row = mysql_fetch_assoc($result);
-			$body = __("A comment has been added to %s, you may view it at:\nhttp://aur.archlinux.org/packages.php?do_Details=1&ID=%s\n\nYou recieved this e-mail because you chose to recieve notifications of new comments on this package, if you no longer wish to recieve notifications about this package, please go the the above package page and click the appropriate control.",array($row['Name'],$_REQUEST["ID"]));
+			$body = __("A comment has been added to %s, you may view it at:\nhttp://aur.archlinux.org/packages.php?do_Details=1&ID=%s\n\nYou received this e-mail because you chose to recieve notifications of new comments on this package, if you no longer wish to recieve notifications about this package, please go the the above package page and click the appropriate control.",array($row['Name'],$_REQUEST["ID"]));
 			$body = wordwrap($body, 70);
 			$bcc = implode(', ', $bcc);
 			$headers = "Bcc: $bcc\nReply-to: nobody@archlinux.org\nFrom:aur-notify@archlinux.org\nX-Mailer: PHP\nX-MimeOLE: Produced By AUR\n";
