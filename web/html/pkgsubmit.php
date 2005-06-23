4666bd8307a5b2716d6e82c3bd47dcd12994b092
pjmattal <pjmattal>
2005-06-23 13:02:29 +0000

    added patch from Jurgen (thanks!)

--- web/html/pkgsubmit.php
+++ web/html/pkgsubmit.php
@@ -164,8 +164,8 @@ if ($_COOKIE["AURSID"]) {
 			}
 		}
 
-		$shcmd = "/bin/sh -c 'mv ".$upload_file." ".$INCOMING_DIR.$pkg_name;
-		$shcmd.= "/".$_FILES["pfile"]["name"]."'";
+    $shcmd = "/bin/mv ".$upload_file." ";
+    $shcmd.= escapeshellarg($INCOMING_DIR.$pkg_name."/".$_FILES["pfile"]["name"]);
 		@exec($shcmd);
 
 		# if no error, get list of directory contents and process PKGBUILD
