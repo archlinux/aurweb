6edbef61c18bd5b652a726e2cb64090e4a59cb97
eliott <eliott@cactuswax.net>
2008-01-16 20:28:02 -0800

    Hacky attempt to fix lang and navigation bars

--- web/template/header.php
+++ web/template/header.php
@@ -1,59 +1,62 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-	<html xmlns="http://www.w3.org/1999/xhtml"
+<html xmlns="http://www.w3.org/1999/xhtml"
 	xml:lang="<?php print "$LANG\" lang=\"$LANG"; ?>">
-	<head>
-  <title>AUR (<?php print $LANG; ?>)<?php if ($title != "") { print " - " . $title; } ?></title>
+  <head>
+    <title>AUR (<?php print $LANG; ?>)<?php if ($title != "") { print " - " . $title; } ?></title>
 	<link rel='stylesheet' type='text/css' href='/css/fonts.css' />
 	<link rel='stylesheet' type='text/css' href='/css/containers.css' />
 	<link rel='stylesheet' type='text/css' href='/css/arch.css' />
 	<link rel='shortcut icon' href='images/favicon.ico' />
 	<link rel='alternate' type='application/rss+xml' title='Newest Packages RSS' href='rss2.php' />
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-   </head>
-   <body bgcolor='white'>
-<div id="head_container">
-   <div id="title">
-      <div id="logo"><h1 id="archtitle"><a href="/" title="Arch Linux (Home)">Arch Linux</a></h1></div>
-   </div>
-   <div id="main_nav">
-      <ul>
-         <li><a href="http://www.archlinux.org/download/">Get Arch</a></li>
-         <li class="selected"><a href="http://aur.archlinux.org">AUR</a></li>
-         <li><a href="http://bugs.archlinux.org">Bugs</a></li>
-         <li><a href="http://wiki.archlinux.org">Wiki</a></li>
-         <li><a href="http://bbs.archlinux.org">Forums</a></li>
-         <li><a href="http://www.archlinux.org">Home</a></li>
-      </ul>
-   </div>
-   <div id="sub_nav">
-      <ul>
+  </head>
+  <body bgcolor='white'>
+    <div id="head_container">
+      <div id="title">
+        <div id="logo"><h1 id="archtitle"><a href="/" title="Arch Linux (Home)">Arch Linux</a></h1></div>
+      </div>
+      <div id="main_nav">
+        <ul>
+          <li><a href="http://www.archlinux.org/download/">Get Arch</a></li>
+          <li class="selected"><a href="http://aur.archlinux.org">AUR</a></li>
+          <li><a href="http://bugs.archlinux.org">Bugs</a></li>
+          <li><a href="http://wiki.archlinux.org">Wiki</a></li>
+          <li><a href="http://bbs.archlinux.org">Forums</a></li>
+          <li><a href="http://www.archlinux.org">Home</a></li>
+        </ul>
+      </div>
+      <div id="sub_nav">
+        <ul>
 <?php
 if (isset($_COOKIE["AURSID"])) {
 ?>
-	<li><a href="/logout.php"><?php print __("Logout"); ?></a></li>
-	<li><a href="/pkgsubmit.php"><?php print __("Submit"); ?></a></li>
-	<li><a href="/packages.php?do_MyPackages=1"><?php print __("My Packages"); ?></a></li>
+	      <li><a href="/logout.php"><?php print __("Logout"); ?></a></li>
+	      <li><a href="/pkgsubmit.php"><?php print __("Submit"); ?></a></li>
+	      <li><a href="/packages.php?do_MyPackages=1"><?php print __("My Packages"); ?></a></li>
 <?php
 	if (account_from_sid($_COOKIE["AURSID"]) == "Trusted User"
 		|| account_from_sid($_COOKIE["AURSID"]) == "Developer") {
 ?>
-	<li><a href="/tu.php"><?php print __("Trusted User"); ?></a></li>
+	      <li><a href="/tu.php"><?php print __("Trusted User"); ?></a></li>
 <?php
 	}
 }
 ?>
-   <li><a href="http://archlinux.org/mailman/listinfo/aur-general">
+          <li><a href="http://archlinux.org/mailman/listinfo/aur-general">
 	<?php print __("Discussion"); ?></a></li>
-	<li><a href="http://bugs.archlinux.org/index.php?tasks=all&project=2"><?php print __("Bugs"); ?></a></li>
-   <li><a href="packages.php"><?php print __("Packages"); ?></a></li>
-   <li><a href="account.php"><?php print __("Accounts"); ?></a></li>
-   <li><a href="index.php">AUR <?php print __("Home"); ?></a></li>
+	      <li><a href="http://bugs.archlinux.org/index.php?tasks=all&project=2"><?php print __("Bugs"); ?></a></li>
+          <li><a href="packages.php"><?php print __("Packages"); ?></a></li>
+          <li><a href="account.php"><?php print __("Accounts"); ?></a></li>
+          <li><a href="index.php">AUR <?php print __("Home"); ?></a></li>
         </ul>
       </div>
-   <div id="lang_sub">
-	 <ul>
+    </div>
+    <div id="lang_login_sub">
+	    <span id="lang_bar">
+        <ul>
+          <li>Lang: </li>
 <?php
 reset($SUPPORTED_LANGS);
 foreach ($SUPPORTED_LANGS as $lang => $lang_name) {
@@ -63,11 +66,9 @@ foreach ($SUPPORTED_LANGS as $lang => $lang_name) {
 		. strtoupper($lang) . "</a></li>\n";
 }
 ?>
-           <li>Lang: </li>
-         </ul>
-      </div>
-			<br />
-      <div style="text-align: right; padding-right: 10px; clear: right">
+        </ul>
+        </span>
+        <span id="login_bar">
 <?php
 if (isset($_COOKIE["AURSID"])) {
   print __("Logged-in as: %h%s%h",
@@ -75,21 +76,19 @@ if (isset($_COOKIE["AURSID"])) {
 } else {
 	if ($login_error) {
 		print "<span class='error'>" . $login_error . "</span><br />\n";
-  }
-?>
+    } ?>
         <form method='post'>
-<?php print __("Username:"); ?>
-        <input type='text' name='user' size='30' maxlength='64'>
-<?php print __("Password:"); ?>
-        <input type='password' name='pass' size='30' maxlength='32'>
-        <input type='submit' class='button' value='<?php  print __("Login"); ?>'>
+         <?php print __("Username:"); ?>
+          <input type='text' name='user' size='30' maxlength='64'>
+         <?php print __("Password:"); ?>
+          <input type='password' name='pass' size='30' maxlength='32'>
+          <input type='submit' class='button' value='<?php  print __("Login"); ?>'>
         </form>
-<?php
-}
-?>
+<?php } ?>
+        </span>
+      </div>
     </div>
-  </div>
-  <div id="maincontent">
+    <div id="maincontent">
 	  <!-- Start of main content -->
 
 
