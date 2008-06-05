a59229b617ed509a88c4a25c029a97256e6fd481
Michael Klier <chi@chimeric.de>
2008-06-05 23:34:26 +0200

    xhtml validation fixes for login_form.php

--- web/template/login_form.php
+++ web/template/login_form.php
@@ -1,28 +1,23 @@
 <span id="login_bar">
-<?php
-if (isset($_COOKIE["AURSID"])) {
-	print __("Logged-in as: %h%s%h",
-		array("<b>", username_from_sid($_COOKIE["AURSID"]), "</b>"));
-}
-else {
-	if ($login_error) {
-		print "<span class='error'>" . $login_error . "</span><br />\n";
-	} ?>
-        <form method='post'>
-         <?php print __("Username:"); ?>
-	<input type='text' name='user' size='30'
-		maxlength="<?php print USERNAME_MAX_LEN; ?>"
-		value='<?php
-			if (isset($_POST['user'])) {
-				print htmlspecialchars($_POST['user'], ENT_QUOTES);
-			 } ?>'>
-         <?php print __("Password:"); ?>
-	<input type='password' name='passwd' size='30'
-		maxlength="<?php print PASSWD_MAX_LEN; ?>">
-	<input type='submit' class='button'
-		 value='<?php  print __("Login"); ?>'>
-</form>
-<?php } ?>
+  <?php
+  if (isset($_COOKIE["AURSID"])) {
+    print __("Logged-in as: %h%s%h", array("<b>", username_from_sid($_COOKIE["AURSID"]), "</b>"));
+  }
+  else {
+    if ($login_error) {
+      print "<span class='error'>" . $login_error . "</span><br />\n";
+    } 
+  ?>
+  <form method="post">
+    <label><?php print __("Username:"); ?></label>
+    <input type="text" name="user" size="30" maxlength="<?php print USERNAME_MAX_LEN; ?>" value="<?php
+      if (isset($_POST['user'])) {
+        print htmlspecialchars($_POST['user'], ENT_QUOTES);
+      } ?>" />
+    <label><?php print __("Password:"); ?></label>
+    <input type="password" name="passwd" size="30" maxlength="<?php print PASSWD_MAX_LEN; ?>" />
+    <input type="submit" class="button" value="<?php  print __("Login"); ?>" />
+  </form>
+  <?php } ?>
 </span>
-
 
