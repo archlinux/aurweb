bbce51e419536070307418605af121ce2b94da60
Callan Barrett <wizzomafizzo@gmail.com>
2008-06-08 23:39:43 +0800

    Fix URL and wording of pkgedit.php email
    Very small fix, added word button to end of body and got rid of do_Details
    in the package URL.
    
    Signed-off-by: Callan Barrett <wizzomafizzo@gmail.com>

--- web/html/pkgedit.php
+++ web/html/pkgedit.php
@@ -91,7 +91,7 @@ if ($_REQUEST["add_Comment"]) {
 			#TODO: native language emails for users, based on their prefs
 			# Simply making these strings translatable won't work, users would be
 			# getting emails in the language that the user who posted the comment was in
-			$body = "A comment has been added to ".$row['Name'].", you may view it at:\nhttp://aur.archlinux.org/packages.php?do_Details=1&ID=".$_REQUEST["ID"]."\n\n\n---\nYou received this e-mail because you chose to recieve notifications of new comments on this package, if you no longer wish to recieve notifications about this package, please go the the above package page and click the UnNotify.";
+			$body = "A comment has been added to ".$row['Name'].", you may view it at:\nhttp://aur.archlinux.org/packages.php?ID=".$_REQUEST["ID"]."\n\n\n---\nYou received this e-mail because you chose to recieve notifications of new comments on this package, if you no longer wish to recieve notifications about this package, please go the the above package page and click the UnNotify button.";
 			$body = wordwrap($body, 70);
 			$bcc = implode(', ', $bcc);
 			$headers = "Bcc: $bcc\nReply-to: nobody@archlinux.org\nFrom:aur-notify@archlinux.org\nX-Mailer: PHP\nX-MimeOLE: Produced By AUR\n";
