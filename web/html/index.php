4d29be001bd78bc5a1b3edd393e9ce2084d0d882
Callan Barrett <wizzomafizzo@gmail.com>
2008-06-12 01:09:38 +0800

    Split html and php in index.php
    No more prints, still all in same file
    
    Signed-off-by: Callan Barrett <wizzomafizzo@gmail.com>

--- web/html/index.php
+++ web/html/index.php
@@ -2,84 +2,20 @@
 
 set_include_path(get_include_path() . PATH_SEPARATOR . '../lib' . PATH_SEPARATOR . '../lang');
 
-# include("index_po.inc");
-include("pkgfuncs_po.inc"); # Add to handle the i18n of My Packages
+# Add to handle the i18n of My Packages
+include("pkgfuncs_po.inc");
 include("aur.inc");
+
 set_lang();
 check_sid();
-html_header(__("Home"));
 
-# Big Top Level Table (Table 1)
-echo "<div class=\"pgbox\">\n";
-echo "  <div class=\"pgboxtitle\"><span class=\"f3\">AUR ".__("Home")."</span></div>\n";
-echo "  <div class=\"frontpgboxbody\">\n";
-echo "<table border='0' cellpadding='0' cellspacing='3' width='90%'>\n";
+html_header( __("Home") );
 
-# MAIN: Top
-echo "<tr>\n";
-print "<td class='boxSoft' valign='top' colspan='2'>";
-print "<p>".__("Welcome to the AUR! Please read the %hAUR User Guidelines%h and %hAUR TU Guidelines%h for more information.", array('<a href="http://wiki.archlinux.org/index.php/AUR_User_Guidelines">', '</a>', '<a href="http://wiki.archlinux.org/index.php/AUR_Trusted_User_Guidelines">', '</a>'))."<br>";
-print __("Contributed PKGBUILDs <b>must</b> conform to the %hArch Packaging Standards%h otherwise they will be deleted!", array('<a href="http://wiki.archlinux.org/index.php/Arch_Packaging_Standards">', '</a>'))."</p>";
-print "<p>".__("Remember to vote for your favourite packages!")."<br>";
-print __("The most popular packages will be provided as binary packages in [community].")."</p>";
-print "</td>";
-print "</tr>";
+# Newest packages
+$q = "SELECT * FROM Packages WHERE DummyPkg != 1 ORDER BY GREATEST(SubmittedTS,ModifiedTS) DESC LIMIT 0 , 10";
+$newest_packages = db_query($q, $dbh);
 
-# MAIN: Bottom Left
-print "<tr>";
-print "<td class='boxSoft' valign='top'>";
-
-#Hey, how about listing the newest pacakges? :D
-$q = "SELECT * FROM Packages ";
-$q.= "WHERE DummyPkg != 1 ";
-$q.= "ORDER BY GREATEST(SubmittedTS,ModifiedTS) DESC ";
-$q.= "LIMIT 0 , 10";
-$result = db_query($q,$dbh);
-# Table 2
-print '<table class="boxSoft">';
-print '<tr>';
-print '<th colspan="2" class="boxSoftTitle">';
-print ' <a href="/rss2.php"><img src="/images/rss.gif"></a> <span class="f3">'.__("Recent Updates").' <span class="f5"></span></span>';
-print '</th>';
-print '</tr>';
-
-while ($row = mysql_fetch_assoc($result)) {
-	print '<tr>';
-        print '<td class="boxSoft">';
-
-        print '<span class="f4"><span class="blue"><a href="/packages.php?ID='.intval($row["ID"]).'">';
-	print $row["Name"]." ".$row["Version"]."</a></span></span>";
-
-        print '</td>';
-	print '<td class="boxSoft">';
-
-        # figure out the mod string
-        $mod_int = intval($row["ModifiedTS"]);
-        $sub_int = intval($row["SubmittedTS"]);
-        if ($mod_int != 0) {
-	  $modstring = gmdate("r", $mod_int);
-        }
-        elseif ($sub_int != 0) {
-          $modstring = '<img src="/images/new.gif"/> '.gmdate("r", $sub_int);
-        }
-        else {
-          $mod_string = "(unknown)";
-        }
-        print '<span class="f4">'.$modstring.'</span>';
-        print '</td>';
-	print '</tr>'."\n";
-}
-print "</td>";
-print "</tr>";
-print "</table>";
-# End Table 2
-
-# MAIN: Bottom Right
-print "</td>";
-print "<td class='boxSoft' valign='top'>";
-
-# AUR STATISTICS 
-
+# AUR statistics
 $q = "SELECT count(*) FROM Packages,PackageLocations WHERE Packages.LocationID = PackageLocations.ID AND PackageLocations.Location = 'unsupported'";
 $result = db_query($q, $dbh);
 $row = mysql_fetch_row($result);
@@ -106,8 +42,6 @@ $result = db_query($q, $dbh);
 $row = mysql_fetch_row($result);
 $update_count = $row[0];
 
-# Added the user statistcs.
-# Added by: dsa <dsandrade@gmail.com>
 $user = username_from_sid($_COOKIE["AURSID"]);
 
 if (!empty($user)) {
@@ -121,22 +55,6 @@ if (!empty($user)) {
     $row = mysql_fetch_row($result);
     $flagged_outdated = $row[0];
     
-    print "<table class='boxSoft'>";
-    
-    print "<tr>";
-    print "<th colspan='2' class='boxSoftTitle'>";
-    print "<span class='f3'>".__("My Statistics")."</span>";
-    print "</th>";
-    print "</tr>";
-    
-    # Number of packages in unsupported
-    print "<tr>";
-    print "<td class='boxSoft'>";
-    print "<span class='f4'>".__("Packages in unsupported")."</span>";
-    print "</td>";
-    print "<td class='boxSoft'><span class='f4'>$maintainer_unsupported_count</span></td>";
-    print "</tr>";
-    
     # If the user is a TU calculate the number of the packages
     $atype = account_from_sid($_COOKIE["AURSID"]);
     
@@ -145,79 +63,178 @@ if (!empty($user)) {
         $result = db_query($q, $dbh);
         $row = mysql_fetch_row($result);
         $maintainer_community_count = $row[0];
-        
-        print "<tr>";
-        print "<td class='boxSoft'>";
-        print "<span class='f4'>".__("Packages in [community]")."</span>";
-        print "</td>";
-        print "<td class='boxSoft'><span class='f4'>$maintainer_community_count</span></td>";
-        print "</tr>";
     }
-    
-    # Number of outdated packages    
-    print "<tr>";
-    print "<td class='boxSoft'>";
-    print "<span class='f4'>".__("Out-of-date")."</span>";
-    print "</td>";
-    print "<td class='boxSoft'><span class='f4'>$flagged_outdated</span></td>";
-    print "</tr>";    
-        
-    print "</table><br />";
 }
 
-print "<table class='boxSoft'>";
+?>
 
-print "<tr>";
-print "<th colspan='2' class='boxSoftTitle'>";
-print "<span class='f3'>".__("Statistics")."</span>";
-print "</th>";
-print "</tr>";
+<div class="pgbox">
+<div class="pgboxtitle">
+<span class="f3">AUR <?php print __("Home"); ?></span>
+</div>
+<div class="frontpgboxbody">
+<table border='0' cellpadding='0' cellspacing='3' width='90%'>
+<tr>
+<td class='boxSoft' valign='top' colspan='2'>
+<p>
 
-print "<tr>";
-print "<td class='boxSoft'>";
-print "<span class='f4'>".__("Packages in unsupported")."</span>";
-print "</td>";
-print "<td class='boxSoft'><span class='f4'>$unsupported_count</span></td>";
-print "</tr>";
+<?php 
+print __( 'Welcome to the AUR! Please read the %hAUR User Guidelines%h and %hAUR TU Guidelines%h for more information.'
+        , '<a href="http://wiki.archlinux.org/index.php/AUR_User_Guidelines">'
+        , '</a>'
+        , '<a href="http://wiki.archlinux.org/index.php/AUR_Trusted_User_Guidelines">'
+        , '</a>'
+        );
+?>
 
-print "<tr>";
-print "<td class='boxSoft'>";
-print "<span class='f4'>".__("Packages in [community]")."</span>";
-print "</td>";
-print "<td class='boxSoft'><span class='f4'>$community_count</span></td>";
-print "</tr>";
+<br>
 
-print "<tr>";
-print "<td class='boxSoft'>";
-print "<span class='f4'>".__("Packages added or updated in the past 7 days")."</span>";
-print "</td>";
-print "<td class='boxSoft'><span class='f4'>$update_count</span></td>";
-print "</tr>";
+<?php
+print __( 'Contributed PKGBUILDs <b>must</b> conform to the %hArch Packaging Standards%h otherwise they will be deleted!'
+        , '<a href="http://wiki.archlinux.org/index.php/Arch_Packaging_Standards">'
+        , '</a>'
+        );
+?>
 
-print "<tr>";
-print "<td class='boxSoft'>";
-print "<span class='blue'><span class='f4'>".__("Registered Users")."</span></span>";
-print "</td>";
-print "<td class='boxSoft'><span class='f4'>$user_count</span></td>";
-print "</tr>";
+</p>
+<p>
+<?php print __("Remember to vote for your favourite packages!"); ?>
+<br>
+<?php print __("The most popular packages will be provided as binary packages in [community]."); ?>
+</p>
+</td>
+</tr>
+<tr>
+<td class='boxSoft' valign='top'>
+<table class="boxSoft">
+<tr>
+<th colspan="2" class="boxSoftTitle" style="text-align: right">
+<a href="/rss2.php"><img src="/images/rss.gif"></a> <span class="f3"><?php print __("Recent Updates") ?><span class="f5"></span></span>
+</th>
+</tr>
 
-print "<tr>";
-print "<td class='boxSoft'>";
-print "<span class='f4'>".__("Trusted Users")."</span>";
-print "</td>";
-print "<td class='boxSoft'><span class='f4'>$tu_count</span></td>";
-print "</tr>";
+<?php while ($row = mysql_fetch_assoc($newest_packages)): ?>
 
-print "</table>";
+<tr>
+<td class="boxSoft">
+<span class="f4"><span class="blue"><a href="/packages.php?ID=<?php print intval($row["ID"]); ?>">
+<?php print $row["Name"] . ' ' . $row["Version"]; ?>
+</a></span>
+</td>
+<td class="boxSoft">
 
-# Close out the right column
-print "  </td>";
-print "</tr>\n";
-print "</table>\n";
-# End Table 1
-echo "<br /><span class='important'>".__("DISCLAIMER")."</span>";
-echo "  </div>";
-echo "</div>";
+<?php
+$mod_int = intval($row["ModifiedTS"]);
+$sub_int = intval($row["SubmittedTS"]);
+
+if ($mod_int != 0):
+  $modstring = gmdate("r", $mod_int);
+elseif ($sub_int != 0):
+  $modstring = '<img src="/images/new.gif"/> ' . gmdate("r", $sub_int);
+else:
+  $modstring = '(unknown)';
+endif;
+?>
+
+<span class="f4"><?php print $modstring; ?></span>
+</td>
+</tr>
+
+<?php endwhile; ?>
+
+</td>
+</tr>
+</table>
+<td class='boxSoft' valign='top'>
+
+<?php if (!empty($user)): ?>
+
+<table class='boxSoft'>
+<tr>
+<th colspan='2' class='boxSoftTitle'>
+<span class='f3'><?php print __("My Statistics"); ?></span>
+</th>
+</tr>
+<tr>
+<td class='boxSoft'>
+<span class='f4'><?php print __("Packages in unsupported"); ?></span>
+</td>
+<td class='boxSoft'>
+<span class='f4'><?php print $maintainer_unsupported_count; ?></span>
+</td>
+</tr>
+
+<?php if ($atype == 'Trusted User'): ?>
+
+<tr>
+<td class='boxSoft'>
+<span class='f4'><?php print __("Packages in [community]"); ?></span>
+</td>
+<td class='boxSoft'>
+<span class='f4'><?php print $maintainer_community_count; ?></span>
+</td>
+</tr>
+
+<?php endif; ?>
+
+<tr>
+<td class='boxSoft'>
+<span class='f4'><?php print __("Out-of-date"); ?></span>
+</td>
+<td class='boxSoft'>
+<span class='f4'><?php print $flagged_outdated ?></span>
+</td>
+</tr>
+</table>
+<br />
+
+<?php endif; ?>
+
+<table class='boxSoft'>
+<tr>
+<th colspan='2' class='boxSoftTitle'>
+<span class='f3'><?php print __("Statistics") ?></span>
+</th>
+</tr>
+<tr>
+<td class='boxSoft'>
+<span class='f4'><?php print __("Packages in unsupported"); ?></span>
+</td>
+<td class='boxSoft'><span class='f4'><?php print $unsupported_count; ?></span></td>
+</tr>
+<tr>
+<td class='boxSoft'>
+<span class='f4'><?php print __("Packages in [community]"); ?></span>
+</td>
+<td class='boxSoft'><span class='f4'><?php print $community_count; ?></span></td>
+</tr>
+<tr>
+<td class='boxSoft'>
+<span class='f4'><?php print __("Packages added or updated in the past 7 days"); ?></span>
+</td>
+<td class='boxSoft'><span class='f4'><?php print $update_count; ?></span></td>
+</tr>
+<tr>
+<td class='boxSoft'>
+<span class='blue'><span class='f4'><?php print __("Registered Users"); ?></span></span>
+</td>
+<td class='boxSoft'><span class='f4'><?php print $user_count; ?></span></td>
+</tr>
+<tr>
+<td class='boxSoft'>
+<span class='f4'><?php print __("Trusted Users"); ?></span>
+</td>
+<td class='boxSoft'><span class='f4'><?php print $tu_count; ?></span></td>
+</tr>
+</table>
+</td>
+</tr>
+</table>
+<br /><span class='important'><?php print __("DISCLAIMER"); ?></span>
+</div>
+</div>
+
+<?php
 html_footer(AUR_VERSION);
 # vim: ts=2 sw=2 noet ft=php
 ?>
