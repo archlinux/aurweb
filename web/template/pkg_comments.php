36b44f371bd240fe448d7a22f7295581c9b35633
Loui Chang <louipc.ist@gmail.com>
2009-01-05 15:59:59 -0500

    Move package comments to a template.
    Change layout in the process.
    
    Signed-off-by: Loui Chang <louipc.ist@gmail.com>

---
+++ web/template/pkg_comments.php
@@ -0,0 +1,38 @@
+<div class="pgbox">
+<table width="100%">
+<?php while (list($indx, $carr) = each($comments)) { ?>
+	<tr class='boxSoft'>
+	<td class='boxSoftTitle' valign='top' style='padding-right: 10' colspan='2'>
+	<span class='f3'>
+<?php
+	if (canDeleteComment($carr['ID'], $atype, $SID)) {
+		$durl = '<a href="pkgedit.php?del_Comment=1';
+		$durl.= '&comment_id=' . $carr['ID'] . '&ID=' . $row['ID'];
+		$durl.= '"><img src="images/x.png" border="0"';
+		$durl.= ' alt="' . __("Delete comment") . '"></a> ';
+
+	  echo $durl;
+	}
+	if ($SID) {
+		echo __("Comment by: %h%s%h on %s",
+			"<a href='account.php?Action=AccountInfo&ID=".$carr["UsersID"]."'><b>",
+			$carr["UserName"], "</b></a>", gmdate("Y m d [H:i:s]", $carr["CommentTS"]));
+	} else {
+		echo __("Comment by: %h%s%h on %s",
+			"<b>", $carr["UserName"], "</b>",
+			gmdate("Y m d [H:i:s]", $carr["CommentTS"]));
+	}
+?>
+	</span>
+	</td>
+	</tr>
+	<tr>
+	<td class="boxSoft">
+	<pre><?php echo htmlspecialchars($carr["Comments"]) ?></pre>
+	</td>
+	</tr>
+<?php
+}
+?>
+</table>
+</div>
