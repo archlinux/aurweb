233f67b87edf0d063c0f56cece50c2f1bb1a31ff
Lukas Fleischer <archlinux@cryptocrack.de>
2011-01-11 10:38:24 +0100

    Do not overwrite package details when adding comments (fixes FS#22075).
    Ensure that the "$row" variable isn't overwritten in
    "web/template/pkg_comment_form.php" during sending mail notifications
    when adding a comment.
    
    Signed-off-by: Lukas Fleischer <archlinux@cryptocrack.de>

--- web/template/pkg_comment_form.php
+++ web/template/pkg_comment_form.php
@@ -25,7 +25,7 @@ if (isset($_REQUEST['comment'])) {
 			array_push($bcc, $row['Email']);
 		}
 
-		$q = 'SELECT Packages.Name ';
+		$q = 'SELECT Packages.* ';
 		$q.= 'FROM Packages ';
 		$q.= 'WHERE Packages.ID = ' . intval($_REQUEST['ID']);
 		$result = db_query($q, $dbh);
