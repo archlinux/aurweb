3d6c6cc98a09bd9d30a144d428d6ff093de2f477
PyroPeter <abi1789@googlemail.com>
2011-02-15 21:32:52 +0100

    Support for langauges written right-to-left
    Signed-off-by: PyroPeter <abi1789@googlemail.com>
    Signed-off-by: Lukas Fleischer <archlinux@cryptocrack.de>

--- web/template/footer.php
+++ web/template/footer.php
@@ -2,7 +2,7 @@
 	<!-- End of main content -->
 <?php
 	if ($ver) {
-		echo "<p class=\"pgbox\" style=\"text-align: right; font-size: 14px;\"><b>$ver</b></p>\n";
+		echo "<div class=\"pgbox version\">$ver</div>";
 	}
 ?>
 	</body>
