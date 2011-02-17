346e82716bf52dec37e7b8c1a62ad30585a8aa58
PyroPeter <abi1789@googlemail.com>
2011-02-17 19:52:10 +0100

    pkg_search_results: replace blind-table with floating divs
    * I tried to remove errors in the sgml-structure
      e.g.: <div>
              <?php if (foo) { ?>
                </div>
              <?php } ?>
    * I did not remove or add code (except the <table> and <div> stuff, of cause).
      I only changed the order of the html/php-tags.
    * The bottom and top of the script are now properly indented.
      I did not indent the middle part (table of search results) because that would
      render the diff completely useless.
    
    Signed-off-by: PyroPeter <abi1789@googlemail.com>
    Signed-off-by: Lukas Fleischer <archlinux@cryptocrack.de>

--- web/template/pkg_search_results.php
+++ web/template/pkg_search_results.php
@@ -1,13 +1,16 @@
-<form action='packages.php?<?php echo htmlentities($_SERVER['QUERY_STRING']) ?>' method='post'>
-<div class="pgbox">
 <?php if (!$result) { ?>
-<div class='pgboxbody'><?php print __("Error retrieving package list.") ?></div>
+	<div class='pgboxbody'><?php print __("Error retrieving package list.") ?></div>
 <?php } elseif ($total == 0) { ?>
-<div class='pgboxbody'><?php print __("No packages matched your search criteria.") ?></div>
+	<div class='pgboxbody'><?php print __("No packages matched your search criteria.") ?></div>
 <?php } else { ?>
-	<div class="pgboxtitle">
-		<span class='f3'><?php print __("Package Listing") ?></span>
-	</div>
+	<form action='packages.php?<?php echo htmlentities($_SERVER['QUERY_STRING']) ?>' method='post'>
+		<div class="pgbox">
+			<div class="pgboxtitle">
+				<span class='f3'><?php print __("Package Listing") ?></span>
+			</div>
+
+
+
 
 <table width='100%' cellspacing='0' cellpadding='2'>
 <tr>
@@ -79,59 +82,52 @@ for ($i = 0; $row = mysql_fetch_assoc($result); $i++) {
 <?php } ?>
 
 	</table>
-</div>
+</div> <!-- .pgbox ??! -->
 
 
-<div class="pgbox">
-	<table width='100%'>
-	<tr>
-		<td align='left'>
-	<div id="legend">
-		<span class='f3'><?php echo __('Legend') ?></span>
-		<span class="outofdate"><?php print __('Out of Date') ?></span>
-	</div>
-	<?php if ($SID): ?>
-	<div>
-		<select name='action'>
-			<option><?php print __("Actions") ?></option>
-			<option value='do_Flag'><?php print __("Flag Out-of-date") ?></option>
-			<option value='do_UnFlag'><?php print __("Unflag Out-of-date") ?></option>
-			<option value='do_Adopt'><?php print __("Adopt Packages") ?></option>
-			<option value='do_Disown'><?php print __("Disown Packages") ?></option>
-			<?php if ($atype == "Trusted User" || $atype == "Developer"): ?>
-			<option value='do_Delete'><?php print __("Delete Packages") ?></option>
-			<?php endif; ?>
-			<option value='do_Notify'><?php print __("Notify") ?></option>
-			<option value='do_UnNotify'><?php print __("UnNotify") ?></option>
-		</select>
-		<?php if ($atype == "Trusted User" || $atype == "Developer"): ?>
-		<input type='checkbox' name='confirm_Delete' value='1' /> <?php print __("Confirm") ?>
-		<?php endif; ?>
-		<input type='submit' class='button' style='width: 80px' value='<?php print __("Go") ?>' />
-	</div>
-	<?php endif; ?>
-		</td>
-
-		<td align='right'>
-			<div class="f4 blue">
-				<?php print __("Showing results %s - %s of %s", $first, $last, $total) ?>
-			</div>
-			<div class="page_nav">
-				<?php foreach($templ_pages as $pagenr => $pagestart) { ?>
-					<?php if ($pagestart === false) { ?>
-						<?php echo $pagenr ?>
-					<?php } else if ($pagestart + 1 == $first) { ?>
-						<span class="page_sel"><?php echo $pagenr ?></span>
-					<?php } else { ?>
-						<a class="page_num" href="packages.php?<?php print mkurl('O=' . (	$pagestart)) ?>"><?php echo $pagenr ?></a>
+		<div class="pgbox pkg_search_results_footer">
+			<div class="legend_and_actions">
+				<div class="legend">
+					<span class='f3'><?php echo __('Legend') ?></span>
+					<span class="outofdate"><?php print __('Out of Date') ?></span>
+				</div>
+				<?php if ($SID): ?>
+					<div>
+						<select name='action'>
+							<option><?php print __("Actions") ?></option>
+							<option value='do_Flag'><?php print __("Flag Out-of-date") ?></option>
+							<option value='do_UnFlag'><?php print __("Unflag Out-of-date") ?></option>
+							<option value='do_Adopt'><?php print __("Adopt Packages") ?></option>
+							<option value='do_Disown'><?php print __("Disown Packages") ?></option>
+							<?php if ($atype == "Trusted User" || $atype == "Developer"): ?>
+							<option value='do_Delete'><?php print __("Delete Packages") ?></option>
+							<?php endif; ?>
+							<option value='do_Notify'><?php print __("Notify") ?></option>
+							<option value='do_UnNotify'><?php print __("UnNotify") ?></option>
+						</select>
+						<?php if ($atype == "Trusted User" || $atype == "Developer"): ?>
+							<input type='checkbox' name='confirm_Delete' value='1' /> <?php print __("Confirm") ?>
+						<?php endif; ?>
+						<input type='submit' class='button' style='width: 80px' value='<?php print __("Go") ?>' />
+					</div>
+				<?php endif; # if ($SID) ?>
+			</div> <!-- .legend_and_actions -->
+			<div class="page_links">
+				<div class="f4 blue">
+					<?php print __("Showing results %s - %s of %s", $first, $last, $total) ?>
+				</div>
+				<div class="page_nav">
+					<?php foreach($templ_pages as $pagenr => $pagestart) { ?>
+						<?php if ($pagestart === false) { ?>
+							<?php echo $pagenr ?>
+						<?php } else if ($pagestart + 1 == $first) { ?>
+							<span class="page_sel"><?php echo $pagenr ?></span>
+						<?php } else { ?>
+							<a class="page_num" href="packages.php?<?php print mkurl('O=' . (	$pagestart)) ?>"><?php echo $pagenr ?></a>
+						<?php } ?>
 					<?php } ?>
-				<?php } ?>
-			</div>
-		</td>
-	</tr>
-
-<?php } ?>
-	</table>
-</div>
-</form>
-
+				</div>
+			</div> <!-- .page_links -->
+		</div> <!-- .pgbox .pkg_search_results_footer -->
+	</form>
+<?php } # search was successful and returned multiple results ?>
