79ce38d271bee57a792aeb8f52e9e96b6899a756
Dan McGee <dan@archlinux.org>
2011-03-30 20:48:10 -0500

    Add indentation to stats table
    Signed-off-by: Dan McGee <dan@archlinux.org>
    Signed-off-by: Lukas Fleischer <archlinux@cryptocrack.de>

--- web/template/stats/general_stats_table.php
+++ web/template/stats/general_stats_table.php
@@ -1,32 +1,21 @@
 <table class='boxSoft'>
-<tr>
-<th colspan='2' class='boxSoftTitle'>
-<span class='f3'><?php print __("Statistics") ?></span>
-</th>
-</tr>
-<tr>
-<td class='boxSoft'>
-<span class='f4'><?php print __("Packages in unsupported"); ?></span>
-</td>
-<td class='boxSoft'><span class='f4'><?php print $unsupported_count; ?></span></td>
-</tr>
-<tr>
-<td class='boxSoft'>
-<span class='f4'><?php print __("Packages added or updated in the past 7 days"); ?></span>
-</td>
-<td class='boxSoft'><span class='f4'><?php print $update_count; ?></span></td>
-</tr>
-<tr>
-<td class='boxSoft'>
-<span class='blue'><span class='f4'><?php print __("Registered Users"); ?></span></span>
-</td>
-<td class='boxSoft'><span class='f4'><?php print $user_count; ?></span></td>
-</tr>
-<tr>
-<td class='boxSoft'>
-<span class='f4'><?php print __("Trusted Users"); ?></span>
-</td>
-<td class='boxSoft'><span class='f4'><?php print $tu_count; ?></span></td>
-</tr>
+	<tr>
+		<th colspan='2' class='boxSoftTitle'><span class='f3'><?php print __("Statistics") ?></span></th>
+	</tr>
+	<tr>
+		<td class='boxSoft'><span class='f4'><?php print __("Packages in unsupported"); ?></span></td>
+		<td class='boxSoft'><span class='f4'><?php print $unsupported_count; ?></span></td>
+	</tr>
+	<tr>
+		<td class='boxSoft'><span class='f4'><?php print __("Packages added or updated in the past 7 days"); ?></span></td>
+		<td class='boxSoft'><span class='f4'><?php print $update_count; ?></span></td>
+	</tr>
+	<tr>
+		<td class='boxSoft'><span class='f4'><?php print __("Registered Users"); ?></span></td>
+		<td class='boxSoft'><span class='f4'><?php print $user_count; ?></span></td>
+	</tr>
+	<tr>
+		<td class='boxSoft'><span class='f4'><?php print __("Trusted Users"); ?></span></td>
+		<td class='boxSoft'><span class='f4'><?php print $tu_count; ?></span></td>
+	</tr>
 </table>
-
