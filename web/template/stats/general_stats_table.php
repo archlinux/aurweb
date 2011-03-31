2005a2889cebc817ad4bae6134aac109165fe595
Dan McGee <dan@archlinux.org>
2011-03-30 20:48:11 -0500

    Add more stats to the front page table
    Signed-off-by: Dan McGee <dan@archlinux.org>
    Signed-off-by: Lukas Fleischer <archlinux@cryptocrack.de>

--- web/template/stats/general_stats_table.php
+++ web/template/stats/general_stats_table.php
@@ -3,12 +3,28 @@
 		<th colspan='2' class='boxSoftTitle'><span class='f3'><?php print __("Statistics") ?></span></th>
 	</tr>
 	<tr>
-		<td class='boxSoft'><span class='f4'><?php print __("Packages in unsupported"); ?></span></td>
+		<td class='boxSoft'><span class='f4'><?php print __("Packages"); ?></span></td>
 		<td class='boxSoft'><span class='f4'><?php print $unsupported_count; ?></span></td>
 	</tr>
 	<tr>
-		<td class='boxSoft'><span class='f4'><?php print __("Packages added or updated in the past 7 days"); ?></span></td>
+		<td class='boxSoft'><span class='f4'><?php print __("Orphan Packages"); ?></span></td>
+		<td class='boxSoft'><span class='f4'><?php print $orphan_count; ?></span></td>
+	</tr>
+	<tr>
+		<td class='boxSoft'><span class='f4'><?php print __("Packages added in the past 7 days"); ?></span></td>
+		<td class='boxSoft'><span class='f4'><?php print $add_count; ?></span></td>
+	</tr>
+	<tr>
+		<td class='boxSoft'><span class='f4'><?php print __("Packages updated in the past 7 days"); ?></span></td>
 		<td class='boxSoft'><span class='f4'><?php print $update_count; ?></span></td>
+	</tr>
+	<tr>
+		<td class='boxSoft'><span class='f4'><?php print __("Packages updated in the past year"); ?></span></td>
+		<td class='boxSoft'><span class='f4'><?php print $update_year_count; ?></span></td>
+	</tr>
+	<tr>
+		<td class='boxSoft'><span class='f4'><?php print __("Packages never updated"); ?></span></td>
+		<td class='boxSoft'><span class='f4'><?php print $never_update_count; ?></span></td>
 	</tr>
 	<tr>
 		<td class='boxSoft'><span class='f4'><?php print __("Registered Users"); ?></span></td>
