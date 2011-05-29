21827dfb1c458edec68e1af4c75bca234f2abfac
Lukas Fleischer <archlinux@cryptocrack.de>
2011-05-29 15:58:46 +0200

    Properly encode ampersands in the RPC usage output
    Signed-off-by: Lukas Fleischer <archlinux@cryptocrack.de>

--- web/html/rpc.php
+++ web/html/rpc.php
@@ -26,14 +26,14 @@ The methods currently allowed are: <br />
   <li>msearch</li>
 </ul> <br />
 Each method requires the following HTTP GET syntax: <br />
-&nbsp;&nbsp; type=<i>methodname</i>&arg=<i>data</i>
+&nbsp;&nbsp; type=<i>methodname</i>&amp;arg=<i>data</i>
 <br /><br />
 Where <i>methodname</i> is the name of an allowed method, and <i>data</i> is the argument to the call.
 <br /><br />
 If you need jsonp type callback specification, you can provide an additional variable <i>callback</i>.
 <br />
 Example URL: <br />
-	&nbsp;&nbsp; http://aur-url/rpc.php?type=search&arg=foobar&callback=jsonp1192244621103
+	&nbsp;&nbsp; http://aur-url/rpc.php?type=search&amp;arg=foobar&amp;callback=jsonp1192244621103
 </body></html>
 <?php
 // close if statement
