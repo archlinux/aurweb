2e20a7929f2a3e2b6470d7e1e4d9dc69c859aaf1
Dan McGee <dan@archlinux.org>
2011-06-27 17:31:32 -0500

    aurblup: use implicit make rules and adjust CFLAGS
    We don't need to add rules for implicit .c -> .o conversions. Also add
    -O2 to the CFLAGS as gcc doesn't print many warnings unless at least
    some level of optimization is used.
    
    Signed-off-by: Dan McGee <dan@archlinux.org>
    Signed-off-by: Lukas Fleischer <archlinux@cryptocrack.de>

--- scripts/aurblup/config.mk
+++ scripts/aurblup/config.mk
@@ -1,6 +1,6 @@
 PREFIX = /usr/local
 
-CFLAGS = -g -std=c99 -pedantic -Wall -I/usr/include/mysql
+CFLAGS = -g -O2 -std=c99 -pedantic -Wall -I/usr/include/mysql
 LDFLAGS = -g -lalpm -lmysqlclient
 
 CC = cc
