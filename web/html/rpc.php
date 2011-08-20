114edd9ff82b3860e2ca4b68589d661ec6d5fe39
Lukas Fleischer <archlinux@cryptocrack.de>
2011-08-20 21:02:44 +0200

    Provide more examples on the RPC info page
    Signed-off-by: Lukas Fleischer <archlinux@cryptocrack.de>

--- web/html/rpc.php
+++ web/html/rpc.php
@@ -18,7 +18,7 @@ else {
 	// here.
 ?>
 <html><body>
-<p>The methods currently allowed are:</p>
+<h2>Allowed methods</h2>
 <ul>
   <li><tt>search</tt></li>
   <li><tt>info</tt></li>
@@ -29,7 +29,14 @@ else {
 <pre>type=<em>methodname</em>&amp;arg=<em>data</em></pre>
 <p>Where <em>methodname</em> is the name of an allowed method, and <em>data</em> is the argument to the call.</p>
 <p>If you need jsonp type callback specification, you can provide an additional variable <em>callback</em>.</p>
-<p>Example URL: <tt>http://aur-url/rpc.php?type=search&amp;arg=foobar&amp;callback=jsonp1192244621103</tt></p>
+<h2>Examples</h2>
+<dl>
+  <dt><tt>search</tt></dt><dd><tt>http://aur-url/rpc.php?type=search&amp;arg=foobar</tt></li></dd>
+  <dt><tt>info</tt></dt><dd><tt>http://aur-url/rpc.php?type=info&amp;arg=foobar</tt></dd>
+  <dt><tt>multiinfo</tt></dt><dd><tt>http://aur-url/rpc.php?type=multiinfo&amp;arg[]=foo&amp;arg[]=bar</tt></dd>
+  <dt><tt>msearch</tt></dt><dd><tt>http://aur-url/rpc.php?type=msearch&amp;arg=john</tt></li></dd>
+  <dt>Callback</dt><dd><tt>http://aur-url/rpc.php?type=search&amp;arg=foobar&amp;callback=jsonp1192244621103</tt></dd>
+</dl>
 </body></html>
 <?php
 // close if statement
