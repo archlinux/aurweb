bfc4a11006eff1909d8129f217f1fe73b471886f
Lukas Fleischer <archlinux@cryptocrack.de>
2012-05-23 13:40:18 -0400

    Synchronize footer layout with archweb
    Also, add a copyright notice and the disclaimer.
    
    Signed-off-by: Lukas Fleischer <archlinux@cryptocrack.de>
    Signed-off-by: canyonknight <canyonknight@gmail.com>

--- web/template/footer.php
+++ web/template/footer.php
@@ -1,10 +1,13 @@
 
-	<!-- End of main content -->
-<?php
-	if ($ver) {
-		echo "<div class=\"pgbox version\">" . htmlspecialchars($ver) . "</div>";
-	}
-?>
+			<!-- End of main content -->
+
+			<div id="footer">
+				<?php if ($ver): ?>
+				<p>AUR <?php echo htmlspecialchars($ver) ?></p>
+				<?php endif; ?>
+				<p>Copyright &copy; 2004-2012 AUR Development Team.</p>
+				<p><?php echo __('Unsupported packages are user produced content. Any use of the provided files is at your own risk.') ?></p>
+			</div>
 		</div>
 	</body>
 </html>
