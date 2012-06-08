16e250b69d575491cd7397496969d3f73c5bd478
canyonknight <canyonknight@gmail.com>
2012-06-08 17:03:21 -0400

    actions_form.php: Overhaul to match archweb
    * Change all CSS to match archweb
    * General clean-up of XHTML formatting
    * Change control structures to use PHP alternative syntax for better
    readability with inter-mixed XHTML
    
    Signed-off-by: canyonknight <canyonknight@gmail.com>
    Signed-off-by: Lukas Fleischer <archlinux@cryptocrack.de>

--- web/template/actions_form.php
+++ web/template/actions_form.php
@@ -1,65 +1,58 @@
-<div class="pgbox">
+<div class="box">
 	<form action="packages.php?ID=<?php echo $row['ID'] ?>" method="post">
 		<fieldset>
-			<input type='hidden' name='IDs[<?php echo $row['ID'] ?>]' value='1' />
-			<input type='hidden' name='ID' value="<?php echo $row['ID'] ?>" />
-<?php
+			<input type="hidden" name="IDs[<?php echo $row['ID'] ?>]" value="1" />
+			<input type="hidden" name="ID" value="<?php echo $row['ID'] ?>" />
+		<?php
 		# Voting Button
 		#
 		$q = "SELECT * FROM PackageVotes WHERE UsersID = ". $uid;
 		$q.= " AND PackageID = ".$row["ID"];
 		$result = db_query($q, $dbh);
-		if ($result) {
-			if (!mysql_num_rows($result)) {
-				echo "      <input type='submit' class='button' name='do_Vote'";
-				echo " value='".__("Vote")."' /> ";
-			} else {
-				echo "<input type='submit' class='button' name='do_UnVote'";
-				echo " value='".__("UnVote")."' /> ";
-			}
-		}
-
+		if ($result):
+			if (!mysql_num_rows($result)):
+		?>
+			<input type="submit" class="button" name="do_Vote" value="<?php echo __("Vote") ?>" />
+			<?php else: ?>
+			<input type="submit" class="button" name="do_UnVote" value="<?php echo __("UnVote") ?>" />
+			<?php endif; ?>
+		<?php endif; ?>
+		<?php
 		# Comment Notify Button
 		#
 		$q = "SELECT * FROM CommentNotify WHERE UserID = ". $uid;
 		$q.= " AND PkgID = ".$row["ID"];
 		$result = db_query($q, $dbh);
-		if ($result) {
-			if (!mysql_num_rows($result)) {
-				echo "<input type='submit' class='button' name='do_Notify'";
-				echo " value='".__("Notify")."' title='".__("New Comment Notification")."' /> ";
-			} else {
-				echo "<input type='submit' class='button' name='do_UnNotify'";
-				echo " value='".__("UnNotify")."' title='".__("No New Comment Notification")."' /> ";
-			}
-		}
+		if ($result):
+			if (!mysql_num_rows($result)):
+		?>
+			<input type="submit" class="button" name="do_Notify" value="<?php echo __("Notify") ?>" title="<?php echo __("New Comment Notification") ?>" />
+			<?php else: ?>
+			<input type="submit" class="button" name="do_UnNotify" value="<?php echo __("UnNotify") ?>" title="<?php echo __("No New Comment Notification") ?>" />
+			<?php endif; ?>
+		<?php endif; ?>
 
-		if ($row["OutOfDateTS"] === NULL) {
-			echo "<input type='submit' class='button' name='do_Flag'";
-			echo " value='".__("Flag Out-of-date")."' />\n";
-		} else {
-			echo "<input type='submit' class='button' name='do_UnFlag'";
-			echo " value='".__("UnFlag Out-of-date")."' />\n";
-		}
+		<?php if ($row["OutOfDateTS"] === NULL): ?>
+			<input type="submit" class="button" name="do_Flag" value="<?php echo __("Flag Out-of-date") ?>" />
+		<?php else: ?>
+			<input type="submit" class="button" name="do_UnFlag" value="<?php echo __("UnFlag Out-of-date") ?>" />
+		<?php endif; ?>
 			
-		if ($row["MaintainerUID"] === NULL) {
-			echo "<input type='submit' class='button' name='do_Adopt'";
-			echo " value='".__("Adopt Packages")."' />\n";
-		} else if ($uid == $row["MaintainerUID"] ||
-			$atype == "Trusted User" || $atype == "Developer") {
-			echo "<input type='submit' class='button' name='do_Disown'";
-			echo " value='".__("Disown Packages")."' />\n";
-		}
-			
-		if ($atype == "Trusted User" || $atype == "Developer") {
-			echo "<input type='submit' class='button' name='do_Delete'";
-			echo " value='".__("Delete Packages")."' />\n";
-			echo "<label for='merge_Into'>".__("Merge into")."</label>\n";
-			echo "<input type='text' id='merge_Into' name='merge_Into' /> ";
-			echo "<input type='checkbox' name='confirm_Delete' value='1' /> ";
-			echo __("Confirm")."\n";
-		}
-?>
+		<?php if ($row["MaintainerUID"] === NULL): ?>
+			<input type="submit" class="button" name="do_Adopt" value="<?php echo __("Adopt Packages") ?>" />
+		<?php elseif ($uid == $row["MaintainerUID"] ||
+			$atype == "Trusted User" || $atype == "Developer"): ?>
+			<input type="submit" class="button" name="do_Disown" value="<?php echo __("Disown Packages") ?>" />
+		<?php endif; ?>
+
+		<?php if ($atype == "Trusted User" || $atype == "Developer"): ?>
+			<input type="submit" class="button" name="do_Delete" value="<?php echo __("Delete Packages") ?>" />
+			<label for="merge_Into" ><?php echo __("Merge into") ?></label>
+			<input type="text" id="merge_Into" name="merge_Into" />
+			<input type="checkbox" name="confirm_Delete" value="1" />
+			<?php echo __("Confirm") ?>
+		<?php endif; ?>
+
 		</fieldset>
 	</form>
 </div>
