10c774f0346b08fc4d720c4e80582476ba7e61a9
Lukas Fleischer <archlinux@cryptocrack.de>
2012-07-17 21:45:03 +0200

    Rearrange package actions
    Move the flag/unflag action below the flag date.
    
    Signed-off-by: Lukas Fleischer <archlinux@cryptocrack.de>

--- web/template/pkg_details.php
+++ web/template/pkg_details.php
@@ -38,6 +38,11 @@ $sources = package_sources($row["ID"]);
 				<li><a href="<?php echo $urlpath . '/' . $row['Name'] ?>.tar.gz"><?php echo __('Download tarball') ?></a></li>
 				<li><span class="flagged"><?php if ($row["OutOfDateTS"] !== NULL) { echo __('Flagged out-of-date')." (${out_of_date_time})"; } ?></span></li>
 				<?php if ($USE_VIRTUAL_URLS && $uid): ?>
+				<?php if ($row["OutOfDateTS"] === NULL): ?>
+				<li><a href="<?php echo get_pkg_uri($row['Name']) . 'flag/'; ?>"><?php echo __('Flag package out-of-date'); ?></a></li>
+				<?php else: ?>
+				<li><a href="<?php echo get_pkg_uri($row['Name']) . 'unflag/'; ?>"><?php echo __('Unflag package'); ?></a></li>
+				<?php endif; ?>
 				<?php if (user_voted($uid, $row['ID'])): ?>
 				<li><a href="<?php echo get_pkg_uri($row['Name']) . 'unvote/'; ?>"><?php echo __('Remove vote'); ?></a></li>
 				<?php else: ?>
@@ -47,11 +52,6 @@ $sources = package_sources($row["ID"]);
 				<li><a href="<?php echo get_pkg_uri($row['Name']) . 'unnotify/'; ?>"><?php echo __('Disable notifications'); ?></a></li>
 				<?php else: ?>
 				<li><a href="<?php echo get_pkg_uri($row['Name']) . 'notify/'; ?>"><?php echo __('Notify of new comments'); ?></a></li>
-				<?php endif; ?>
-				<?php if ($row["OutOfDateTS"] === NULL): ?>
-				<li><a href="<?php echo get_pkg_uri($row['Name']) . 'flag/'; ?>"><?php echo __('Flag package out-of-date'); ?></a></li>
-				<?php else: ?>
-				<li><a href="<?php echo get_pkg_uri($row['Name']) . 'unflag/'; ?>"><?php echo __('Unflag package'); ?></a></li>
 				<?php endif; ?>
 				<?php endif; ?>
 			</ul>
