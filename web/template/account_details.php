7519028d12f9a78048a0a808fa715c54d13f02d9
Lukas Fleischer <archlinux@cryptocrack.de>
2012-09-19 11:33:33 +0200

    Sync account details page with archweb
    Synchronize the layout of the account details page with the developer
    profiles page from archweb.
    
    Signed-off-by: Lukas Fleischer <archlinux@cryptocrack.de>

--- web/template/account_details.php
+++ web/template/account_details.php
@@ -1,57 +1,55 @@
-<table>
+<table class="arch-bio-entry">
 	<tr>
-		<td colspan="2">&nbsp;</td>
-	</tr>
-
-	<tr>
-		<td align="left"><?= __("Username") . ":" ?></td>
-		<td align="left"><?= $row["Username"] ?></td>
-	</tr>
-
-	<tr>
-		<td align="left"><?= __("Account Type") . ":" ?></td>
-		<td align="left">
-			<?php
-			if ($row["AccountType"] == "User") {
-				print __("User");
-			} elseif ($row["AccountType"] == "Trusted User") {
-				print __("Trusted User");
-			} elseif ($row["AccountType"] == "Developer") {
-				print __("Developer");
-			}
-			?>
+		<td>
+			<h3><?= htmlspecialchars($row["Username"], ENT_QUOTES) ?></h3>
+			<table class="bio">
+				<tr>
+					<th><?= __("Username") . ":" ?></td>
+					<td><?= $row["Username"] ?></td>
+				</tr>
+				<tr>
+					<th><?= __("Account Type") . ":" ?></td>
+					<td>
+						<?php
+						if ($row["AccountType"] == "User") {
+							print __("User");
+						} elseif ($row["AccountType"] == "Trusted User") {
+							print __("Trusted User");
+						} elseif ($row["AccountType"] == "Developer") {
+							print __("Developer");
+						}
+						?>
+					</td>
+				</tr>
+				<tr>
+					<th><?= __("Email Address") . ":" ?></td>
+					<td><a href="mailto:<?= htmlspecialchars($row["Email"], ENT_QUOTES) ?>"><?= htmlspecialchars($row["Email"], ENT_QUOTES) ?></a></td>
+				</tr>
+				<tr>
+					<th><?= __("Real Name") . ":" ?></td>
+					<td><?= htmlspecialchars($row["RealName"], ENT_QUOTES) ?></td>
+				</tr>
+				<tr>
+					<th><?= __("IRC Nick") . ":" ?></td>
+					<td><?= htmlspecialchars($row["IRCNick"], ENT_QUOTES) ?></td>
+				</tr>
+				<tr>
+					<th><?= __("PGP Key Fingerprint") . ":" ?></td>
+					<td><?= html_format_pgp_fingerprint($row["PGPKey"]) ?></td>
+				</tr>
+				<tr>
+					<th><?= __("Last Voted") . ":" ?></td>
+					<td>
+					<?= $row["LastVoted"] ? date("Y-m-d", $row["LastVoted"]) : __("Never"); ?>
+					</td>
+				</tr>
+				<tr>
+					<th>Links:</th>
+					<td>
+						<a href="<?= get_uri('/packages/'); ?>?K=<?= $row['Username'] ?>&amp;SeB=m"><?= __("View this user's packages") ?></a>
+					</td>
+				</tr>
+			</table>
 		</td>
 	</tr>
-
-	<tr>
-		<td align="left"><?= __("Email Address") . ":" ?></td>
-		<td align="left"><a href="mailto:<?= htmlspecialchars($row["Email"], ENT_QUOTES) ?>"><?= htmlspecialchars($row["Email"], ENT_QUOTES) ?></a></td>
-	</tr>
-
-	<tr>
-		<td align="left"><?= __("Real Name") . ":" ?></td>
-		<td align="left"><?= htmlspecialchars($row["RealName"], ENT_QUOTES) ?></td>
-	</tr>
-
-	<tr>
-		<td align="left"><?= __("IRC Nick") . ":" ?></td>
-		<td align="left"><?= htmlspecialchars($row["IRCNick"], ENT_QUOTES) ?></td>
-	</tr>
-
-	<tr>
-		<td align="left"><?= __("PGP Key Fingerprint") . ":" ?></td>
-		<td align="left"><?= html_format_pgp_fingerprint($row["PGPKey"]) ?></td>
-	</tr>
-
-	<tr>
-		<td align="left"><?= __("Last Voted") . ":" ?></td>
-		<td align="left">
-		<?= $row["LastVoted"] ? date("Y-m-d", $row["LastVoted"]) : __("Never"); ?>
-		</td>
-	</tr>
-
-	<tr>
-		<td colspan="2"><a href="<?= get_uri('/packages/'); ?>?K=<?= $row['Username'] ?>&amp;SeB=m"><?= __("View this user's packages") ?></a></td>
-	</tr>
-
 </table>
