65cc6d3e2dbb9847ac8751983226c79abc574dc7
Lukas Fleischer <archlinux@cryptocrack.de>
2012-09-19 11:43:16 +0200

    Sync account editing page with archweb
    Synchronize the layout of the account editing page with the profiles
    page from archweb.
    
    Signed-off-by: Lukas Fleischer <archlinux@cryptocrack.de>

--- web/template/account_edit_form.php
+++ web/template/account_edit_form.php
@@ -1,7 +1,7 @@
 <?php if ($A == "UpdateAccount"): ?>
-<form action="<?= get_user_uri($U) . 'update/'; ?>" method="post">
+<form id="edit-profile-form" action="<?= get_user_uri($U) . 'update/'; ?>" method="post">
 <?php else: ?>
-<form action="<?= get_uri('/register/'); ?>" method="post">
+<form id="edit-profile-form" action="<?= get_uri('/register/'); ?>" method="post">
 <?php endif; ?>
 	<fieldset>
 		<input type="hidden" name="Action" value="<?= $A ?>" />
@@ -10,106 +10,84 @@
 		<input type="hidden" name="token" value="<?= htmlspecialchars($_COOKIE['AURSID']) ?>" />
 		<?php endif; ?>
 	</fieldset>
-	<table>
-		<tr>
-			<td colspan="2">&nbsp;</td>
-		</tr>
-
-		<tr>
-			<td align="left"><?= __("Username") ?>:</td>
-			<td align="left"><input type="text" size="30" maxlength="64" name="U" value="<?= htmlspecialchars($U,ENT_QUOTES) ?>" /> (<?= __("required") ?>)</td>
-		</tr>
+	<fieldset>
+		<p>
+			<label for="id_username"><?= __("Username") ?>:</label>
+			<input type="text" size="30" maxlength="64" name="U" id="id_username" value="<?= htmlspecialchars($U,ENT_QUOTES) ?>" /> (<?= __("required") ?>)
+		</p>
 		<?php
 		# Only TUs or Devs can promote/demote/suspend a user
 		if ($UTYPE == "Trusted User" || $UTYPE == "Developer"):
 		?>
-		<tr>
-			<td align="left"><?= __("Account Type") ?>:</td>
-			<td align="left">
-				<select name=T>
-					<?php if ($T == "User"): ?>
-					<option value="1" selected><?= __("Normal user") ?>
-					<?php else: ?>
-					<option value="1"><?= __("Normal user") ?>
-					<?php endif; ?>
-					<?php if ($T == "Trusted User"): ?>
-					<option value="2" selected><?= __("Trusted user") ?>
-					<?php else: ?>
-					<option value="2"><?= __("Trusted user") ?>
-					<?php endif; ?>
-					<?php
-					# Only developers can make another account a developer
-					if ($UTYPE == "Developer"):
-					?>
-					<option value="3"
-					<?php $T == "Developer" ? print " selected>" : print ">";
-					print __("Developer")."\n"; ?>
-					<?php endif; ?>
-				</select>
-			</td>
-		</tr>
+		<p>
+			<label for="id_type"><?= __("Account Type") ?>:</label>
+			<select name="T" id="id_type">
+				<?php if ($T == "User"): ?>
+				<option value="1" selected><?= __("Normal user") ?>
+				<?php else: ?>
+				<option value="1"><?= __("Normal user") ?>
+				<?php endif; ?>
+				<?php if ($T == "Trusted User"): ?>
+				<option value="2" selected><?= __("Trusted user") ?>
+				<?php else: ?>
+				<option value="2"><?= __("Trusted user") ?>
+				<?php endif; ?>
+				<?php
+				# Only developers can make another account a developer
+				if ($UTYPE == "Developer"):
+				?>
+				<option value="3"
+				<?php $T == "Developer" ? print " selected>" : print ">";
+				print __("Developer")."\n"; ?>
+				<?php endif; ?>
+			</select>
+		</p>
 
-		<tr>
-			<td align="left"><?= __("Account Suspended") ?>:</td>
-
+		<p>
+			<label for="id_suspended"><?= __("Account Suspended") ?>:</label>
 			<?php if ($S): ?>
-			<td align="left"><input type="checkbox" name="S" checked="checked" />
+			<input type="checkbox" name="S" id="id_suspended" checked="checked" />
 			<?php else: ?>
-			<td align="left"><input type="checkbox" name="S" />
+			<input type="checkbox" name="S" id="id_suspended" />
 			<?php endif; ?>
-		</tr>
+		</p>
 		<?php endif; ?>
 
-		<tr>
-			<td align="left"><?= __("Email Address") ?>:</td>
-			<td align="left"><input type="text" size="30" maxlength="64" name="E" value="<?= htmlspecialchars($E,ENT_QUOTES) ?>" /> (<?= __("required") ?>)</td>
-		</tr>
+		<p>
+			<label for="id_email"><?= __("Email Address") ?>:</label>
+			<input type="text" size="30" maxlength="64" name="E" id="id_email" value="<?= htmlspecialchars($E,ENT_QUOTES) ?>" /> (<?= __("required") ?>)
+		</p>
 
-		<tr>
-			<td align="left"><?= __("Password") ?>:</td>
-			<td align="left">
-				<input type="password" size="30" maxlength="32" name="P" value="<?= $P ?>" />
-				<?php if ($A != "UpdateAccount"):
-					print " (".__("required").")";
-				endif; ?>
-			</td>
-		</tr>
+		<p>
+			<label for="id_passwd1"><?= __("Password") ?>:</label>
+			<input type="password" size="30" maxlength="32" name="P" id="id_passwd1" value="<?= $P ?>" />
+			<?php if ($A != "UpdateAccount"): print " (".__("required").")"; endif; ?>
+		</p>
 
-		<tr>
-			<td align="left"><?= __("Re-type password") ?>:</td>
-			<td align="left">
-				<input type="password" size="30" maxlength="32" name="C" value="<?= $C ?>" />
-				<?php if ($A != "UpdateAccount"):
-					print " (".__("required").")";
-				endif; ?>
-			</td>
-		</tr>
+		<p>
+			<label for="id_passwd2"><?= __("Re-type password") ?>:</label>
+			<input type="password" size="30" maxlength="32" name="C" id="id_passwd2" value="<?= $C ?>" />
+			<?php if ($A != "UpdateAccount"): print " (".__("required").")"; endif; ?>
+		</p>
 
-		<tr>
-			<td align="left"><?= __("Real Name") ?>:</td>
-			<td align="left">
-				<input type="text" size="30" maxlength="32" name="R" value="<?= htmlspecialchars($R,ENT_QUOTES) ?>" />
-			</td>
-		</tr>
+		<p>
+			<label for="id_realname"><?= __("Real Name") ?>:</label>
+			<input type="text" size="30" maxlength="32" name="R" id="id_realname" value="<?= htmlspecialchars($R,ENT_QUOTES) ?>" />
+		</p>
 
-		<tr>
-			<td align="left"><?= __("IRC Nick") ?>:</td>
-			<td align="left">
-				<input type="text" size="30" maxlength="32" name="I" value="<?= htmlspecialchars($I,ENT_QUOTES) ?>" />
-			</td>
-		</tr>
+		<p>
+			<label for="id_irc"><?= __("IRC Nick") ?>:</label>
+			<input type="text" size="30" maxlength="32" name="I" id="id_irc" value="<?= htmlspecialchars($I,ENT_QUOTES) ?>" />
+		</p>
 
-		<tr>
-			<td align="left"><?= __("PGP Key Fingerprint") ?>:</td>
-<td align="left">
-				<input type="text" size="30" maxlength="50" name="K" value="<?= html_format_pgp_fingerprint($K) ?>" />
-			</td>
-		</tr>
+		<p>
+			<label for="id_pgp"><?= __("PGP Key Fingerprint") ?>:</label>
+			<input type="text" size="30" maxlength="50" name="K" id="id_pgp" value="<?= html_format_pgp_fingerprint($K) ?>" />
+		</p>
 
-		<tr>
-			<td align="left"><?= __("Language") ?>:</td>
-			<td align="left">
-				<select name=L>
+		<p>
+			<label for="id_language"><?= __("Language") ?>:</label>
+			<select name="L" id="id_language">
 <?php
 	reset($SUPPORTED_LANGS);
 	while (list($code, $lang) = each($SUPPORTED_LANGS)) {
@@ -120,24 +98,17 @@
 		}
 	}
 ?>
-				</select>
-			</td>
-		</tr>
+			</select>
+		</p>
 
-		<tr>
-			<td colspan="2">&nbsp;</td>
-		</tr>
-		<tr>
-			<td>&nbsp;</td>
-			<td align="left">
-				<?php if ($A == "UpdateAccount"): ?>
-				<input type="submit" class="button" value="<?= __("Update") ?>" /> &nbsp;
-				<?php else: ?>
-				<input type="submit" class="button" value="<?= __("Create") ?>" /> &nbsp;
-				<?php endif; ?>
-				<input type="reset" class="button" value="<?= __("Reset") ?>" />
-			</td>
-		</tr>
-
-	</table>
+		<p>
+			<label></label>
+			<?php if ($A == "UpdateAccount"): ?>
+			<input type="submit" class="button" value="<?= __("Update") ?>" /> &nbsp;
+			<?php else: ?>
+			<input type="submit" class="button" value="<?= __("Create") ?>" /> &nbsp;
+			<?php endif; ?>
+			<input type="reset" class="button" value="<?= __("Reset") ?>" />
+		</p>
+	</fieldset>
 </form>
