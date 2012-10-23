6897bb00979238b80d6aa3ad3741d1856ea81c6a
Lukas Fleischer <archlinux@cryptocrack.de>
2012-10-23 19:08:29 +0200

    Implement word-wrap for package statistics
    Word-wrap labels in the package statistics box, just as we wrap package
    names in the "Recent Updates" box.
    
    Addresses FS#32160.
    
    Signed-off-by: Lukas Fleischer <archlinux@cryptocrack.de>

--- web/template/stats/general_stats_table.php
+++ web/template/stats/general_stats_table.php
@@ -2,35 +2,35 @@
 
 <table>
 	<tr>
-		<td><?= __("Packages"); ?></td>
+		<td class="stat-desc"><?= __("Packages"); ?></td>
 		<td><?= $unsupported_count; ?></td>
 	</tr>
 	<tr>
-		<td><?= __("Orphan Packages"); ?></td>
+		<td class="stat-desc"><?= __("Orphan Packages"); ?></td>
 		<td><?= $orphan_count; ?></td>
 	</tr>
 	<tr>
-		<td><?= __("Packages added in the past 7 days"); ?></td>
+		<td class="stat-desc"><?= __("Packages added in the past 7 days"); ?></td>
 		<td><?= $add_count; ?></td>
 	</tr>
 	<tr>
-		<td><?= __("Packages updated in the past 7 days"); ?></td>
+		<td class="stat-desc"><?= __("Packages updated in the past 7 days"); ?></td>
 		<td><?= $update_count; ?></td>
 	</tr>
 	<tr>
-		<td><?= __("Packages updated in the past year"); ?></td>
+		<td class="stat-desc"><?= __("Packages updated in the past year"); ?></td>
 		<td><?= $update_year_count; ?></td>
 	</tr>
 	<tr>
-		<td><?= __("Packages never updated"); ?></td>
+		<td class="stat-desc"><?= __("Packages never updated"); ?></td>
 		<td><?= $never_update_count; ?></td>
 	</tr>
 	<tr>
-		<td><?= __("Registered Users"); ?></td>
+		<td class="stat-desc"><?= __("Registered Users"); ?></td>
 		<td><?= $user_count; ?></td>
 	</tr>
 	<tr>
-		<td><?= __("Trusted Users"); ?></td>
+		<td class="stat-desc"><?= __("Trusted Users"); ?></td>
 		<td><?= $tu_count; ?></td>
 	</tr>
 </table>
