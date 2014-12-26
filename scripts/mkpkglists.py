f55e3c21c6a6ab01bd94c90a8910c44b7596660a
Lukas Fleischer <archlinux@cryptocrack.de>
2014-12-26 22:01:30 +0100

    mkpkglists.py: Honor database socket setting
    Signed-off-by: Lukas Fleischer <archlinux@cryptocrack.de>

--- scripts/mkpkglists.py
+++ scripts/mkpkglists.py
@@ -15,10 +15,11 @@ aur_db_host = config.get('database', 'host')
 aur_db_name = config.get('database', 'name')
 aur_db_user = config.get('database', 'user')
 aur_db_pass = config.get('database', 'password')
+aur_db_socket = config.get('database', 'socket')
 
 db = mysql.connector.connect(host=aur_db_host, user=aur_db_user,
                              passwd=aur_db_pass, db=aur_db_name,
-                             buffered=True)
+                             unix_socket=aur_db_socket, buffered=True)
 cur = db.cursor()
 
 datestr = datetime.datetime.utcnow().strftime("%a, %d %b %Y %H:%M:%S GMT")
