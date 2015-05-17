75923205afaba81968caf2e805c079b4f94a75d5
Lukas Fleischer <lfleischer@archlinux.org>
2015-05-17 11:48:07 +0200

    Fix email body of password reset emails
    When the password reset function was factored out in 5d31bb2 (Move reset
    key submission to a separate function, 2013-03-19), a semicolon was
    accidentally replaced by a concatenation operator. As a result of this,
    all password reset emails sent since mid-2013 only contained a password
    reset link without any description. Fix this by terminating the
    assignment with a semicolon again.
    
    Signed-off-by: Lukas Fleischer <lfleischer@archlinux.org>

--- web/html/passreset.php
+++ web/html/passreset.php
@@ -49,7 +49,7 @@ if (isset($_GET['resetkey'], $_POST['email'], $_POST['password'], $_POST['confir
 		$body = __('A password reset request was submitted for the account '.
 			   'associated with your e-mail address. If you wish to reset '.
 			   'your password follow the link below, otherwise ignore '.
-			   'this message and nothing will happen.').
+			   'this message and nothing will happen.');
 		send_resetkey($email, $subject, $body);
 
 		header('Location: ' . get_uri('/passreset/') . '?step=confirm');
