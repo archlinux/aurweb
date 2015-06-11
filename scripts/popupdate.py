0345914c5ce5e8ecafe3e90280bd58c0255fd9e0
Lukas Fleischer <lfleischer@archlinux.org>
2015-06-11 18:33:32 +0200

    Add popupdate script
    This adds a simple script that can be used to recalculate the number of
    votes per package. It can be used in a cronjob.
    
    Signed-off-by: Lukas Fleischer <lfleischer@archlinux.org>

---
+++ scripts/popupdate.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+import configparser
+import mysql.connector
+import os
+
+docroot = os.path.dirname(os.path.realpath(__file__)) + "/../web/html/"
+
+config = configparser.RawConfigParser()
+config.read(os.path.dirname(os.path.realpath(__file__)) + "/../conf/config")
+
+aur_db_host = config.get('database', 'host')
+aur_db_name = config.get('database', 'name')
+aur_db_user = config.get('database', 'user')
+aur_db_pass = config.get('database', 'password')
+aur_db_socket = config.get('database', 'socket')
+
+db = mysql.connector.connect(host=aur_db_host, user=aur_db_user,
+                             passwd=aur_db_pass, db=aur_db_name,
+                             unix_socket=aur_db_socket, buffered=True)
+cur = db.cursor()
+
+cur.execute("UPDATE PackageBases SET NumVotes = (SELECT COUNT(*) FROM " +
+            "PackageVotes WHERE PackageVotes.PackageBaseID = PackageBases.ID)")
+
+db.commit()
+db.close()
