37960beea130699969e4870799d073f28bc0f550
Johannes Löthberg <johannes@kyriasis.com>
2015-06-14 19:11:47 +0200

    footer: Link version number to release log
    Fixes FS#45321
    
    Signed-off-by: Johannes Löthberg <johannes@kyriasis.com>
    Signed-off-by: Lukas Fleischer <lfleischer@archlinux.org>

--- web/template/footer.php
+++ web/template/footer.php
@@ -3,7 +3,7 @@
 
 			<div id="footer">
 				<?php if ($ver): ?>
-				<p>aurweb <?= htmlspecialchars($ver) ?></p>
+				<p>aurweb <a href="https://projects.archlinux.org/aurweb.git/log/?h=<?= htmlspecialchars($ver, ENT_QUOTES) ?>"><?= htmlspecialchars($ver) ?></a></p>
 				<?php endif; ?>
 				<p>Copyright &copy; 2004-<?= date("Y") ?> aurweb Development Team.</p>
 				<p><?= __('Unsupported packages are user produced content. Any use of the provided files is at your own risk.') ?></p>
