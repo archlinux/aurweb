693e4b50a3fb57f90d802806fd1b8b78f21bd499
Lukas Fleischer <lfleischer@archlinux.org>
2015-09-29 20:47:24 +0200

    Remove empty package bases after 24 hours
    By using the setup-repo command, it is currently possible to create
    empty package bases, which can be used to make package base
    reservations. Add a maintenance script to remove such empty package
    bases after 24 hours.
    
    Fixes FS#46279.
    
    Signed-off-by: Lukas Fleischer <lfleischer@archlinux.org>

---
+++ scripts/pkgmaint.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+import configparser
+import mysql.connector
+import os
+
+config = configparser.RawConfigParser()
+config.read(os.path.dirname(os.path.realpath(__file__)) + "/../conf/config")
+
+aur_db_host = config.get('database', 'host')
+aur_db_name = config.get('database', 'name')
+aur_db_user = config.get('database', 'user')
+aur_db_pass = config.get('database', 'password')
+aur_db_socket = config.get('database', 'socket')
+
+db = mysql.connector.connect(host=aur_db_host, user=aur_db_user,
+                             passwd=aur_db_pass, db=aur_db_name,
+                             unix_socket=aur_db_socket, buffered=True)
+cur = db.cursor()
+
+cur.execute("DELETE FROM PackageBases WHERE " +
+            "UNIX_TIMESTAMP() - SubmittedTS > 86400 AND PackagerUID IS NULL")
+
+db.commit()
+db.close()
