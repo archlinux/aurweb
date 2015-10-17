fd6ba76431c428679f964fef8f82912efad680f5
Lukas Fleischer <lfleischer@archlinux.org>
2015-10-17 11:58:27 +0200

    Make copyright notice translatable
    Fixes FS#46747.
    
    Signed-off-by: Lukas Fleischer <lfleischer@archlinux.org>

--- web/template/footer.php
+++ web/template/footer.php
@@ -5,7 +5,7 @@
 				<?php if ($ver): ?>
 				<p>aurweb <a href="https://projects.archlinux.org/aurweb.git/log/?h=<?= htmlspecialchars($ver, ENT_QUOTES) ?>"><?= htmlspecialchars($ver) ?></a></p>
 				<?php endif; ?>
-				<p>Copyright &copy; 2004-<?= date("Y") ?> aurweb Development Team.</p>
+				<p><?= __('Copyright %s 2004-%d aurweb Development Team.', '&copy;', date('Y'))</p>
 				<p><?= __('AUR packages are user produced content. Any use of the provided files is at your own risk.') ?></p>
 			</div>
 		</div>
