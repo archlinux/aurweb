archlinux/aurweb

commit d273ee5eb20ecb6e97d5d6cd8a1f493e3652b584

Use the official provider list to detect duplicates
Instead of automatically adding packages from the official binary
repositories to the package blacklist, use the official provider list to
prevent users from uploading duplicates.

This does not only result in reduced disk usage but also has a nice
visible side effect. The error messages printed by the update hook now
look like

    error: package already provided by [community]: powerline-fonts

instead of

    error: package is blacklisted: powerline-fonts

which was confusing to most end users.

Signed-off-by: Lukas Fleischer <lfleischer@archlinux.org>

git-interface/git-update.py
@@ -331,12 +331,18 @@ pkgbase_id = cur.fetchone()[0] if cur.rowcount == 1 else 0
 cur.execute("SELECT Name FROM PackageBlacklist")
 blacklist = [row[0] for row in cur.fetchall()]
 
+cur.execute("SELECT Name, Repo FROM OfficialProviders")
+providers = dict(cur.fetchall())
+
 for pkgname in srcinfo.utils.get_package_names(metadata):
     pkginfo = srcinfo.utils.get_merged_package(pkgname, metadata)
     pkgname = pkginfo['pkgname']
 
     if pkgname in blacklist and not privileged:
         die('package is blacklisted: {:s}'.format(pkgname))
+    if pkgname in providers and not privileged:
+        repo = providers[pkgname]
+        die('package already provided by [{:s}]: {:s}'.format(repo, pkgname))
 
     cur.execute("SELECT COUNT(*) FROM Packages WHERE Name = %s AND " +
                 "PackageBaseID <> %s", [pkgname, pkgbase_id])

scripts/aurblup.py
@@ -45,14 +45,6 @@ db = mysql.connector.connect(host=aur_db_host, user=aur_db_user,
                              unix_socket=aur_db_socket, buffered=True)
 cur = db.cursor()
 
-cur.execute("SELECT Name FROM PackageBlacklist")
-oldblacklist = set([row[0] for row in cur.fetchall()])
-
-for pkg in blacklist.difference(oldblacklist):
-    cur.execute("INSERT INTO PackageBlacklist (Name) VALUES (%s)", [pkg])
-for pkg in oldblacklist.difference(blacklist):
-    cur.execute("DELETE FROM PackageBlacklist WHERE Name = %s", [pkg])
-
 cur.execute("SELECT Name, Provides FROM OfficialProviders")
 oldproviders = set(cur.fetchall())
 

commit e3bcf83feb2dd68352c6c47aa16d64c025e1fb5b

git-update: Do not overwrite the repo variable
The repo variable is already used to store the pygit2.Repository.

Fixes a regression introduced in d273ee5 (Use the official provider list
to detect duplicates, 2016-05-17).

Signed-off-by: Lukas Fleischer <lfleischer@archlinux.org>

git-interface/git-update.py
@@ -373,8 +373,7 @@ for pkgname in srcinfo.utils.get_package_names(metadata):
     if pkgname in blacklist:
         warn_or_die('package is blacklisted: {:s}'.format(pkgname))
     if pkgname in providers:
-        repo = providers[pkgname]
-        warn_or_die('package already provided by [{:s}]: {:s}'.format(repo, pkgname))
+        warn_or_die('package already provided by [{:s}]: {:s}'.format(providers[pkgname], pkgname))
 
     cur = conn.execute("SELECT COUNT(*) FROM Packages WHERE Name = ? AND " +
                        "PackageBaseID <> ?", [pkgname, pkgbase_id])

git-interface/test/t0003-update.sh
@@ -396,7 +396,7 @@ test_expect_success 'Pushing a package already in the official repositories.' '
 	test_cmp expected actual
 '
 
-test_expect_failure 'Pushing a package already in the official repositories as Trusted User.' '
+test_expect_success 'Pushing a package already in the official repositories as Trusted User.' '
 	old=$(git -C aur.git rev-parse HEAD) &&
 	test_when_finished "git -C aur.git reset --hard $old" &&
 	echo "pkgname = official" >>aur.git/.SRCINFO &&