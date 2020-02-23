e4cbe264cf6949f82338b7de705cba90c15f4d24
Frédéric Mangano-Tarumi <fmang@mg0.fr>
2020-02-23 19:52:12 +0100

    Create an initial Alembic migration
    This way the database will get stamped, and Git will create the
    `versions` directory without which Alembic won’t work.
    
    Signed-off-by: Lukas Fleischer <lfleischer@archlinux.org>

---
+++ migrations/versions/f47cad5d6d03_initial_revision.py
@@ -0,0 +1,24 @@
+"""initial revision
+
+Revision ID: f47cad5d6d03
+Revises: 
+Create Date: 2020-02-23 13:23:32.331396
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'f47cad5d6d03'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
