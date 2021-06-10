5de7ff64df0dc33a20999ae7042ff04a77451819
Kevin Morris <kevr@0cost.org>
2021-06-10 13:55:07 -0700

    add PackageVote SQLAlchemy ORM model
    Signed-off-by: Kevin Morris <kevr@0cost.org>

---
+++ test/test_package_vote.py
@@ -0,0 +1,58 @@
+from datetime import datetime
+
+import pytest
+
+from sqlalchemy.exc import IntegrityError
+
+from aurweb.db import create, query, rollback
+from aurweb.models.account_type import AccountType
+from aurweb.models.package_base import PackageBase
+from aurweb.models.package_vote import PackageVote
+from aurweb.models.user import User
+from aurweb.testing import setup_test_db
+
+user = pkgbase = None
+
+
+@pytest.fixture(autouse=True)
+def setup():
+    global user, pkgbase
+
+    setup_test_db("Users", "PackageBases", "PackageVotes")
+
+    account_type = query(AccountType,
+                         AccountType.AccountType == "User").first()
+    print(account_type.ID)
+    print(account_type.AccountType)
+
+    user = create(User, Username="test", Email="test@example.org",
+                  RealName="Test User", Passwd="testPassword")
+    pkgbase = create(PackageBase, Name="test-package", Maintainer=user)
+
+
+def test_package_vote_creation():
+    ts = int(datetime.utcnow().timestamp())
+    package_vote = create(PackageVote, User=user, PackageBase=pkgbase,
+                          VoteTS=ts)
+    assert bool(package_vote)
+    assert package_vote.User == user
+    assert package_vote.PackageBase == pkgbase
+    assert package_vote.VoteTS == ts
+
+
+def test_package_vote_null_user_raises_exception():
+    with pytest.raises(IntegrityError):
+        create(PackageVote, PackageBase=pkgbase, VoteTS=1)
+    rollback()
+
+
+def test_package_vote_null_pkgbase_raises_exception():
+    with pytest.raises(IntegrityError):
+        create(PackageVote, User=user, VoteTS=1)
+    rollback()
+
+
+def test_package_vote_null_votets_raises_exception():
+    with pytest.raises(IntegrityError):
+        create(PackageVote, User=user, PackageBase=pkgbase)
+    rollback()
