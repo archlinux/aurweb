96d1af936381a959090d5362ee2ee49b4c91eced
Kevin Morris <kevr@0cost.org>
2021-06-28 08:30:12 -0700

    docker-compose: add redis service
    Now, the fastapi docker-compose service uses the new redis
    service for a cache option.
    
    Signed-off-by: Kevin Morris <kevr@0cost.org>

--- docker/fastapi-entrypoint.sh
+++ docker/fastapi-entrypoint.sh
@@ -7,6 +7,10 @@ bash $dir/test-mysql-entrypoint.sh
 sed -ri "s;^(aur_location) = .+;\1 = https://localhost:8444;" conf/config
 sed -ri 's/^(name) = .+/\1 = aurweb/' conf/config
 
+# Setup Redis for FastAPI.
+sed -ri 's/^(cache) = .+/\1 = redis/' conf/config
+sed -ri 's|^(redis_address) = .+|\1 = redis://redis|' conf/config
+
 sed -ri "s|^(git_clone_uri_anon) = .+|\1 = https://localhost:8444/%s.git|" conf/config.defaults
 sed -ri "s|^(git_clone_uri_priv) = .+|\1 = ssh://aur@localhost:2222/%s.git|" conf/config.defaults
 
--- docker/scripts/install-deps.sh
+++ docker/scripts/install-deps.sh
@@ -14,6 +14,7 @@ pacman -Syu --noconfirm --noprogressbar \
     python-pytest-asyncio python-coverage hypercorn python-bcrypt \
     python-email-validator openssh python-lxml mariadb mariadb-libs \
     python-isort flake8 cgit uwsgi uwsgi-plugin-cgi php php-fpm \
-    python-asgiref uvicorn python-feedgen memcached php-memcached
+    python-asgiref uvicorn python-feedgen memcached php-memcached \
+    python-redis redis
 
 exec "$@"
