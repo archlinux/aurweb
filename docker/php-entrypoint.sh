3bacfe6cd97049926893a37595bbb40158be7a48
Kevin Morris <kevr@0cost.org>
2021-06-29 10:29:24 -0700

    Docker: increase nginx and php-fpm logging
    Log toward stdout/stderr which is accessible via
    `docker-compose logs <service>`.
    
    Examples:
    
    - `docker-compose logs nginx`
    - `docker-compose logs php-fpm`
    
    Signed-off-by: Kevin Morris <kevr@0cost.org>

--- docker/php-entrypoint.sh
+++ docker/php-entrypoint.sh
@@ -13,6 +13,11 @@ sed -ri "s|^(git_clone_uri_priv) = .+|\1 = ssh://aur@localhost:2222/%s.git|" con
 sed -ri 's/^(listen).*/\1 = 0.0.0.0:9000/' /etc/php/php-fpm.d/www.conf
 sed -ri 's/^;?(clear_env).*/\1 = no/' /etc/php/php-fpm.d/www.conf
 
+# Log to stderr. View logs via `docker-compose logs php-fpm`.
+sed -ri 's|^(error_log) = .*$|\1 = /proc/self/fd/2|g' /etc/php/php-fpm.conf
+sed -ri 's|^;?(access\.log) = .*$|\1 = /proc/self/fd/2|g' \
+    /etc/php/php-fpm.d/www.conf
+
 sed -ri 's/^;?(extension=pdo_mysql)/\1/' /etc/php/php.ini
 sed -ri 's/^;?(open_basedir).*$/\1 = \//' /etc/php/php.ini
 
