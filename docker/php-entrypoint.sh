a6ca345af4dbfd018d86fb6725759f20c7cb0c11
Kevin Morris <kevr@0cost.org>
2021-07-14 13:27:15 -0700

    Docker: Fix git clone url in fastapi/php-fpm
    Signed-off-by: Kevin Morris <kevr@0cost.org>
    
    Docker: fix php-entrypoint git clone uri
    
    Signed-off-by: Kevin Morris <kevr@0cost.org>

--- docker/php-entrypoint.sh
+++ docker/php-entrypoint.sh
@@ -7,7 +7,7 @@ bash $dir/test-mysql-entrypoint.sh
 sed -ri "s;^(aur_location) = .+;\1 = https://localhost:8443;" conf/config
 sed -ri 's/^(name) = .+/\1 = aurweb/' conf/config
 
-sed -ri "s|^(git_clone_uri_anon) = .+|\1 = https://localhost:8443/cgit/aur.git -b %s|" conf/config.defaults
+sed -ri "s|^(git_clone_uri_anon) = .+|\1 = https://localhost:8443/%s.git|" conf/config.defaults
 sed -ri "s|^(git_clone_uri_priv) = .+|\1 = ssh://aur@localhost:2222/%s.git|" conf/config.defaults
 
 sed -ri 's/^(listen).*/\1 = 0.0.0.0:9000/' /etc/php/php-fpm.d/www.conf
