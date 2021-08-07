5bd3a7bbabd06bf846991a9dd4c74d61c9a8d0a0
Kevin Morris <kevr@0cost.org>
2021-08-07 15:43:44 -0700

    RequestType: add name_display() and record constants
    Just like some of the other tables, we have some constant
    records that we use to denote types of things. This commit
    adds constants which correlate with these record constants.
    
    Signed-off-by: Kevin Morris <kevr@0cost.org>

--- aurweb/models/request_type.py
+++ aurweb/models/request_type.py
@@ -1,6 +1,11 @@
 from sqlalchemy import Column, Integer
 
+from aurweb import db
 from aurweb.models.declarative import Base
+
+DELETION = "deletion"
+ORPHAN = "orphan"
+MERGE = "merge"
 
 
 class RequestType(Base):
@@ -9,3 +14,13 @@ class RequestType(Base):
     ID = Column(Integer, primary_key=True)
 
     __mapper_args__ = {"primary_key": [ID]}
+
+    def name_display(self) -> str:
+        """ Return the Name column with its first char capitalized. """
+        name = self.Name
+        return name[0].upper() + name[1:]
+
+
+DELETION_ID = db.query(RequestType, RequestType.Name == DELETION).first().ID
+ORPHAN_ID = db.query(RequestType, RequestType.Name == ORPHAN).first().ID
+MERGE_ID = db.query(RequestType, RequestType.Name == MERGE).first().ID
--- test/test_request_type.py
+++ test/test_request_type.py
@@ -1,7 +1,7 @@
 import pytest
 
-from aurweb.db import create, delete
-from aurweb.models.request_type import RequestType
+from aurweb.db import create, delete, query
+from aurweb.models.request_type import DELETION_ID, MERGE_ID, ORPHAN_ID, RequestType
 from aurweb.testing import setup_test_db
 
 
@@ -22,3 +22,14 @@ def test_request_type_null_name_returns_empty_string():
     assert bool(request_type.ID)
     assert request_type.Name == str()
     delete(RequestType, RequestType.ID == request_type.ID)
+
+
+def test_request_type_name_display():
+    deletion = query(RequestType, RequestType.ID == DELETION_ID).first()
+    assert deletion.name_display() == "Deletion"
+
+    orphan = query(RequestType, RequestType.ID == ORPHAN_ID).first()
+    assert orphan.name_display() == "Orphan"
+
+    merge = query(RequestType, RequestType.ID == MERGE_ID).first()
+    assert merge.name_display() == "Merge"
