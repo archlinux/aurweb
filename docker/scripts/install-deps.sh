85b1a05d0138888e2f06c8fc5b474314abd090e9
Hunter Wittenborn <hunter@hunterwittenborn.com>
2021-08-28 19:51:05 -0500

    Removed pip dependencies from docker/scripts/install-deps.sh

--- docker/scripts/install-deps.sh
+++ docker/scripts/install-deps.sh
@@ -5,16 +5,12 @@
 set -eou pipefail
 
 pacman -Syu --noconfirm --noprogressbar \
-    --cachedir .pkg-cache git gpgme protobuf pyalpm \
-    python-mysqlclient python-pygit2 python-srcinfo python-bleach \
-    python-markdown python-sqlalchemy python-alembic python-pytest \
-    python-werkzeug python-pytest-tap python-fastapi nginx python-authlib \
-    python-itsdangerous python-httpx python-jinja python-pytest-cov \
-    python-requests python-aiofiles python-python-multipart \
-    python-pytest-asyncio python-coverage hypercorn python-bcrypt \
-    python-email-validator openssh python-lxml mariadb mariadb-libs \
-    python-isort flake8 cgit uwsgi uwsgi-plugin-cgi php php-fpm \
-    python-asgiref uvicorn python-feedgen memcached php-memcached \
-    python-redis redis python-fakeredis python-orjson
+    --cachedir .pkg-cache git gpgme \
+      nginx redis openssh \
+      mariadb mariadb-libs \
+      cgit uwsgi uwsgi-plugin-cgi \
+      php php-fpm \
+      memcached php-memcached \
+      pyalpm python-srcinfo
 
 exec "$@"
