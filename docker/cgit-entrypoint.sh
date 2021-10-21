db730ad8cbd3944f42fac6941137b4070c794a18
Kevin Morris <kevr@0cost.org>
2021-10-20 17:31:52 -0700

    fix(docker): fix cgit clone-prefix
    Additionally, clone-prefix is now configurable via environment variables:
    
    - CGIT_CLONE_PREFIX_PHP
    - CGIT_CLONE_PREFIX_FASTAPI
    
    These vars can be used by production to customize the clone prefix.
    
    Signed-off-by: Kevin Morris <kevr@0cost.org>

--- docker/cgit-entrypoint.sh
+++ docker/cgit-entrypoint.sh
@@ -4,7 +4,7 @@ set -eou pipefail
 mkdir -p /var/cache/cgit
 
 cp -vf conf/cgitrc.proto /etc/cgitrc
-sed -ri "s|clone-prefix=.*|clone-prefix=${2}|" /etc/cgitrc
+sed -ri "s|clone-prefix=.*|clone-prefix=${CGIT_CLONE_PREFIX}|" /etc/cgitrc
 sed -ri 's|header=.*|header=/aurweb/web/template/cgit/header.html|' /etc/cgitrc
 sed -ri 's|footer=.*|footer=/aurweb/web/template/cgit/footer.html|' /etc/cgitrc
 sed -ri 's|repo\.path=.*|repo.path=/aurweb/aur.git|' /etc/cgitrc
