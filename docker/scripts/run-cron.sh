068b067e148cb4f39fdfb7aa2086568084ac6a0a
Kevin Morris <kevr@0cost.org>
2021-11-09 02:28:52 -0800

    feat(docker): log cron executions
    Signed-off-by: Kevin Morris <kevr@0cost.org>

--- docker/scripts/run-cron.sh
+++ docker/scripts/run-cron.sh
@@ -2,6 +2,13 @@
 
 cd /aurweb
 aurweb-aurblup
+if [ $? -eq 0 ]; then
+    echo "[$(date -u)] executed aurblup" >> /var/log/aurblup.log
+fi
+
 aurweb-mkpkglists
+if [ $? -eq 0 ]; then
+    echo "[$(date -u)] executed mkpkglists" >> /var/log/mkpkglists.log
+fi
 
 exec /usr/bin/crond -n
