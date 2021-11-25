67a6b8360eccb02ac4a65940fde7fa6801ac2398
Kevin Morris <kevr@0cost.org>
2021-11-24 20:33:42 -0800

    fix(docker): remove update and build steps from poetry
    `install` includes dependencies present in poetry.lock
    and we must stick to them if we wish to pin dependencies.
    
    Signed-off-by: Kevin Morris <kevr@0cost.org>

--- docker/scripts/install-python-deps.sh
+++ docker/scripts/install-python-deps.sh
@@ -6,8 +6,6 @@ pip install --upgrade pip
 
 # Install the aurweb package and deps system-wide via poetry.
 poetry config virtualenvs.create false
-poetry update
-poetry build
 poetry install --no-interaction --no-ansi
 
 exec "$@"
