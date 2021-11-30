604df50b88ac6a1b7babfeebb7ee2ae2844fba2a
Kevin Morris <kevr@0cost.org>
2021-11-30 01:49:35 -0800

    housekeep(fastapi): rewrite test_package_comment with fixtures
    Signed-off-by: Kevin Morris <kevr@0cost.org>

--- test/test_package_comment.py
+++ test/test_package_comment.py
@@ -8,21 +8,29 @@ from aurweb.models.package_base import PackageBase
 from aurweb.models.package_comment import PackageComment
 from aurweb.models.user import User
 
-user = pkgbase = None
-
 
 @pytest.fixture(autouse=True)
 def setup(db_test):
-    global user, pkgbase
+    return
 
+
+@pytest.fixture
+def user() -> User:
     with db.begin():
         user = db.create(User, Username="test", Email="test@example.org",
                          RealName="Test User", Passwd="testPassword",
                          AccountTypeID=USER_ID)
+    yield user
+
+
+@pytest.fixture
+def pkgbase(user: User) -> PackageBase:
+    with db.begin():
         pkgbase = db.create(PackageBase, Name="test-package", Maintainer=user)
+    yield pkgbase
 
 
-def test_package_comment_creation():
+def test_package_comment_creation(user: User, pkgbase: PackageBase):
     with db.begin():
         package_comment = db.create(PackageComment, PackageBase=pkgbase,
                                     User=user, Comments="Test comment.",
@@ -30,26 +38,28 @@ def test_package_comment_creation():
     assert bool(package_comment.ID)
 
 
-def test_package_comment_null_package_base_raises_exception():
+def test_package_comment_null_pkgbase_raises(user: User):
     with pytest.raises(IntegrityError):
         PackageComment(User=user, Comments="Test comment.",
                        RenderedComment="Test rendered comment.")
 
 
-def test_package_comment_null_user_raises_exception():
+def test_package_comment_null_user_raises(pkgbase: PackageBase):
     with pytest.raises(IntegrityError):
         PackageComment(PackageBase=pkgbase,
                        Comments="Test comment.",
                        RenderedComment="Test rendered comment.")
 
 
-def test_package_comment_null_comments_raises_exception():
+def test_package_comment_null_comments_raises(user: User,
+                                              pkgbase: PackageBase):
     with pytest.raises(IntegrityError):
         PackageComment(PackageBase=pkgbase, User=user,
                        RenderedComment="Test rendered comment.")
 
 
-def test_package_comment_null_renderedcomment_defaults():
+def test_package_comment_null_renderedcomment_defaults(user: User,
+                                                       pkgbase: PackageBase):
     with db.begin():
         record = db.create(PackageComment, PackageBase=pkgbase,
                            User=user, Comments="Test comment.")
