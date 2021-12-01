150c944758bb67f97625c566cd635500d27ef7ef
Kevin Morris <kevr@0cost.org>
2021-11-30 20:45:08 -0800

    housekeep(fastapi): rewrite test_package_group with fixtures
    Signed-off-by: Kevin Morris <kevr@0cost.org>

--- test/test_package_group.py
+++ test/test_package_group.py
@@ -10,36 +10,48 @@ from aurweb.models.package_base import PackageBase
 from aurweb.models.package_group import PackageGroup
 from aurweb.models.user import User
 
-user = group = pkgbase = package = None
-
 
 @pytest.fixture(autouse=True)
 def setup(db_test):
-    global user, group, pkgbase, package
+    return
 
+
+@pytest.fixture
+def user() -> User:
     with db.begin():
         user = db.create(User, Username="test", Email="test@example.org",
                          RealName="Test User", Passwd="testPassword",
                          AccountTypeID=USER_ID)
-        group = db.create(Group, Name="Test Group")
+    yield user
 
+
+@pytest.fixture
+def group() -> Group:
+    with db.begin():
+        group = db.create(Group, Name="Test Group")
+    yield group
+
+
+@pytest.fixture
+def package(user: User) -> Package:
     with db.begin():
         pkgbase = db.create(PackageBase, Name="test-package", Maintainer=user)
         package = db.create(Package, PackageBase=pkgbase, Name=pkgbase.Name)
+    yield package
 
 
-def test_package_group():
+def test_package_group(package: Package, group: Group):
     with db.begin():
         package_group = db.create(PackageGroup, Package=package, Group=group)
     assert package_group.Group == group
     assert package_group.Package == package
 
 
-def test_package_group_null_package_raises_exception():
+def test_package_group_null_package_raises(group: Group):
     with pytest.raises(IntegrityError):
         PackageGroup(Group=group)
 
 
-def test_package_group_null_group_raises_exception():
+def test_package_group_null_group_raises(package: Package):
     with pytest.raises(IntegrityError):
         PackageGroup(Package=package)
