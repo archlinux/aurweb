140f9b1fb225e00163662290de426e8c2a064264
Kevin Morris <kevr@0cost.org>
2021-11-30 20:50:17 -0800

    housekeep(fastapi): rewrite test_package_dependency with fixtures
    Signed-off-by: Kevin Morris <kevr@0cost.org>

--- test/test_package_dependency.py
+++ test/test_package_dependency.py
@@ -10,28 +10,32 @@ from aurweb.models.package_base import PackageBase
 from aurweb.models.package_dependency import PackageDependency
 from aurweb.models.user import User
 
-user = pkgbase = package = None
-
 
 @pytest.fixture(autouse=True)
 def setup(db_test):
-    global user, pkgbase, package
+    return
 
+
+@pytest.fixture
+def user() -> User:
     with db.begin():
         user = db.create(User, Username="test", Email="test@example.org",
-                         RealName="Test User", Passwd="testPassword",
+                         RealName="Test User", Passwd=str(),
                          AccountTypeID=USER_ID)
-        pkgbase = db.create(PackageBase,
-                            Name="test-package",
-                            Maintainer=user)
-        package = db.create(Package,
-                            PackageBase=pkgbase,
-                            Name=pkgbase.Name,
+    yield user
+
+
+@pytest.fixture
+def package(user: User) -> Package:
+    with db.begin():
+        pkgbase = db.create(PackageBase, Name="test-package", Maintainer=user)
+        package = db.create(Package, PackageBase=pkgbase, Name=pkgbase.Name,
                             Description="Test description.",
                             URL="https://test.package")
+    yield package
 
 
-def test_package_dependencies():
+def test_package_dependencies(user: User, package: Package):
     with db.begin():
         pkgdep = db.create(PackageDependency, Package=package,
                            DepTypeID=DEPENDS_ID, DepName="test-dep")
@@ -57,16 +61,16 @@ def test_package_dependencies():
     assert pkgdep.is_package()
 
 
-def test_package_dependencies_null_package_raises_exception():
+def test_package_dependencies_null_package_raises():
     with pytest.raises(IntegrityError):
         PackageDependency(DepTypeID=DEPENDS_ID, DepName="test-dep")
 
 
-def test_package_dependencies_null_dependency_type_raises_exception():
+def test_package_dependencies_null_dependency_type_raises(package: Package):
     with pytest.raises(IntegrityError):
         PackageDependency(Package=package, DepName="test-dep")
 
 
-def test_package_dependencies_null_depname_raises_exception():
+def test_package_dependencies_null_depname_raises(package: Package):
     with pytest.raises(IntegrityError):
         PackageDependency(DepTypeID=DEPENDS_ID, Package=package)
