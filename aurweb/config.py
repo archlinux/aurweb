07e479ab503b6e2cb2e363ccf731c3ea60281451
Kevin Morris <kevr@0cost.org>
2022-02-23 14:37:41 -0800

    upgrade: bump to v6.0.19
    Signed-off-by: Kevin Morris <kevr@0cost.org>

--- aurweb/config.py
+++ aurweb/config.py
@@ -6,7 +6,7 @@ from typing import Any
 # Publicly visible version of aurweb. This is used to display
 # aurweb versioning in the footer and must be maintained.
 # Todo: Make this dynamic/automated.
-AURWEB_VERSION = "v6.0.18"
+AURWEB_VERSION = "v6.0.19"
 
 _parser = None
 
