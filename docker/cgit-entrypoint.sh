ad61c443f47686f41315735341cf53b4464cf0e0
moson-mo <mo-son@mailbox.org>
2023-04-29 09:55:54 +0200

    fix: restore & move cgit html files
    restore files accidentally deleted with PHP cleanup.
    
    https://gitlab.archlinux.org/archlinux/aurweb/-/tree/1325c71712a12c529d7a3defa9cbabfad296922e/web/template/cgit
    Signed-off-by: moson-mo <mo-son@mailbox.org>

--- docker/cgit-entrypoint.sh
+++ docker/cgit-entrypoint.sh
@@ -5,8 +5,8 @@ mkdir -p /var/data/cgit
 
 cp -vf conf/cgitrc.proto /etc/cgitrc
 sed -ri "s|clone-prefix=.*|clone-prefix=${CGIT_CLONE_PREFIX}|" /etc/cgitrc
-sed -ri 's|header=.*|header=/aurweb/web/template/cgit/header.html|' /etc/cgitrc
-sed -ri 's|footer=.*|footer=/aurweb/web/template/cgit/footer.html|' /etc/cgitrc
+sed -ri 's|header=.*|header=/aurweb/static/html/cgit/header.html|' /etc/cgitrc
+sed -ri 's|footer=.*|footer=/aurweb/static/html/cgit/footer.html|' /etc/cgitrc
 sed -ri 's|repo\.path=.*|repo.path=/aurweb/aur.git|' /etc/cgitrc
 sed -ri "s|^(css)=.*$|\1=${CGIT_CSS}|" /etc/cgitrc
 
