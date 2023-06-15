32461f28eaf786b34d9ee3a8a27d97ee1356228a
moson-mo <mo-son@mailbox.org>
2023-06-15 14:16:38 +0200

    fix(docker): Suppress error PEP-668
    When using docker (compose), we don't create a venv and just install
    python packages system-wide.
    
    With python 3.11 (PEP 668) we need to explicitly tell pip to allow this.
    
    Signed-off-by: moson-mo <mo-son@mailbox.org>

--- docker/scripts/install-python-deps.sh
+++ docker/scripts/install-python-deps.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 set -eou pipefail
 
-# Upgrade PIP; Arch Linux's version of pip is outdated for Poetry.
-pip install --upgrade pip
-
 if [ ! -z "${COMPOSE+x}" ]; then
+    export PIP_BREAK_SYSTEM_PACKAGES=1
     poetry config virtualenvs.create false
 fi
 poetry install --no-interaction --no-ansi
