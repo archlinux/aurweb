3e3173b5c96ca93381e9afeacb5029cc89586547
Leonidas Spyropoulos <artafinde@archlinux.org>
2025-01-12 20:32:57 +0000

    chore: avoid cache for new pacman 7
    Pacman 7 introduced sandboxing which breaks cache in containers due to permissions on containers
    
    Signed-off-by: Leonidas Spyropoulos <artafinde@archlinux.org>

--- docker/scripts/install-deps.sh
+++ docker/scripts/install-deps.sh
@@ -13,7 +13,7 @@ pacman -Sy --noconfirm --noprogressbar archlinux-keyring
 
 # Install other OS dependencies.
 pacman -Syu --noconfirm --noprogressbar \
-    --cachedir .pkg-cache git gpgme nginx redis openssh \
+    git gpgme nginx redis openssh \
     mariadb mariadb-libs cgit-aurweb uwsgi uwsgi-plugin-cgi \
     python-pip pyalpm python-srcinfo curl libeatmydata cronie \
     python-poetry python-poetry-core step-cli step-ca asciidoc \
