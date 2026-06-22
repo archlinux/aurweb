50d37f72429f2d0211678d1a7298fe46375650c5
Jelle van der Waa <jelle@archlinux.org>
2026-06-22 09:33:00 +0200

    fix(aurweb): Use super() instead of super(__class__, self)
    Since Python 3 calling `super()` is equal to `super(ClassName, self)`.

--- aurweb/exceptions.py
+++ aurweb/exceptions.py
@@ -19,66 +19,66 @@ class BannedException(AurwebException):
 class PermissionDeniedException(AurwebException):
     def __init__(self, user):
         msg = f"permission denied: {user:s}"
-        super(PermissionDeniedException, self).__init__(msg)
+        super().__init__(msg)
 
 
 class BrokenUpdateHookException(AurwebException):
     def __init__(self, cmd):
         msg = f"broken update hook: {cmd:s}"
-        super(BrokenUpdateHookException, self).__init__(msg)
+        super().__init__(msg)
 
 
 class InvalidUserException(AurwebException):
     def __init__(self, user):
         msg = f"unknown user: {user:s}"
-        super(InvalidUserException, self).__init__(msg)
+        super().__init__(msg)
 
 
 class InvalidPackageBaseException(AurwebException):
     def __init__(self, pkgbase):
         msg = f"package base not found: {pkgbase:s}"
-        super(InvalidPackageBaseException, self).__init__(msg)
+        super().__init__(msg)
 
 
 class InvalidRepositoryNameException(AurwebException):
     def __init__(self, pkgbase):
         msg = f"invalid repository name: {pkgbase:s}"
-        super(InvalidRepositoryNameException, self).__init__(msg)
+        super().__init__(msg)
 
 
 class PackageBaseExistsException(AurwebException):
     def __init__(self, pkgbase):
         msg = f"package base already exists: {pkgbase:s}"
-        super(PackageBaseExistsException, self).__init__(msg)
+        super().__init__(msg)
 
 
 class InvalidReasonException(AurwebException):
     def __init__(self, reason):
         msg = f"invalid reason: {reason:s}"
-        super(InvalidReasonException, self).__init__(msg)
+        super().__init__(msg)
 
 
 class InvalidCommentException(AurwebException):
     def __init__(self, comment):
         msg = f"comment is too short: {comment:s}"
-        super(InvalidCommentException, self).__init__(msg)
+        super().__init__(msg)
 
 
 class AlreadyVotedException(AurwebException):
     def __init__(self, comment):
         msg = f"already voted for package base: {comment:s}"
-        super(AlreadyVotedException, self).__init__(msg)
+        super().__init__(msg)
 
 
 class NotVotedException(AurwebException):
     def __init__(self, comment):
         msg = f"missing vote for package base: {comment:s}"
-        super(NotVotedException, self).__init__(msg)
+        super().__init__(msg)
 
 
 class InvalidArgumentsException(AurwebException):
     def __init__(self, msg):
-        super(InvalidArgumentsException, self).__init__(msg)
+        super().__init__(msg)
 
 
 class RPCError(AurwebException):
